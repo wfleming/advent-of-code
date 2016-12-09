33b076b11d80aaa238c74e49673247b059710235
Will Fleming <will@flemi.ng>
2016-12-09 18:22:49 -0500

    I forgot to do part 2 of day 1, I guess

--- app/D1.hs
+++ app/D1.hs
@@ -12,32 +12,46 @@ main = do
   let movements = either (error . show) id parseResult
   let finalPos = walk (0,0) movements
   putStrLn $ printf "Walked to " ++ show finalPos ++ " total distance = " ++ show (dist (0,0) finalPos)
+  let dupPos = hitTwice ((0, 0), North) (expand movements) []
+  putStrLn $ printf "first loc seen twice is " ++ show dupPos ++ " total distance = " ++ show (dist (0,0) dupPos)
 
 type Point = (Int, Int)
-data Movement = TurnRight Int | TurnLeft Int
+type Distance = Int
+data Turn = TurnRight | TurnLeft | Straight
+type Movement = (Turn, Distance)
 data Orientation = North | South | West | East
 
 walk :: Point -> [Movement] -> Point
 walk start moves = fst $ foldl step (start, North) moves
+
+hitTwice :: (Point, Orientation) -> [Movement] -> [Point] -> Point
+hitTwice (start, _) [] placesBeen = error $ "exhausted moves without seeing any point twice placesBeen=" ++ show (start : placesBeen)
+hitTwice (start, facing) (nextMove : restMoves) placesBeen =
+    case elem start placesBeen of
+        True -> start
+        False -> hitTwice (step (start, facing) nextMove) restMoves (start : placesBeen)
+
+expand :: [Movement] -> [Movement]
+expand moves = concat $ map expandMove moves
   where
-    step (pos, facing) move =
-        (newPos, newDir)
-      where
-        newDir = turn facing move
-        moveDist = extractMoveDist move
-        newPos = performMove pos newDir moveDist
+    expandMove (t, n) | n >= 1 = (t, 1) : replicate (n - 1) (Straight, 1)
+    expandMove _ = error "accidental negative maybe?"
 
-    extractMoveDist (TurnRight d) = d
-    extractMoveDist (TurnLeft d) = d
+step :: (Point, Orientation) -> Movement -> (Point, Orientation)
+step (pos, facing) (moveTurn, moveDist) = (newPos, newDir)
+  where
+    newDir = turn facing moveTurn
+    newPos = performMove pos newDir moveDist
 
-    turn North (TurnRight _) = East
-    turn North (TurnLeft _) = West
-    turn South (TurnRight _) = West
-    turn South (TurnLeft _) = East
-    turn West (TurnRight _) = North
-    turn West (TurnLeft _) = South
-    turn East (TurnRight _) = South
-    turn East (TurnLeft _) = North
+    turn North TurnRight = East
+    turn North TurnLeft = West
+    turn South TurnRight = West
+    turn South TurnLeft = East
+    turn West TurnRight = North
+    turn West TurnLeft = South
+    turn East TurnRight = South
+    turn East TurnLeft = North
+    turn dir Straight = dir
 
     performMove (x, y) North d = (x, y + d)
     performMove (x, y) South d = performMove (x, y) North (0 - d)
@@ -54,8 +68,8 @@ parseMovements = parseMovement `sepBy` (string ", ")
 -- A movement is a direction & an integral distance
 parseMovement :: Parser Movement
 parseMovement = choice
-    [ movementParser 'R' TurnRight
-    , movementParser 'L' TurnLeft
+    [ movementParser 'R' (\d -> (TurnRight, d))
+    , movementParser 'L' (\d -> (TurnLeft, d))
     ]
 
 movementParser :: Char -> (Int -> Movement) -> Parser Movement
