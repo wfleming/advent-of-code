3d1f0609d9b07bc6553087543dcb38197984c904
Will Fleming <will@flemi.ng>
2017-12-10 22:48:40 -0500

    2017 day 10 part 2

--- 2017/D10/app/Main.hs
+++ 2017/D10/app/Main.hs
@@ -9,6 +9,9 @@ main = do
   parseResult <- parseFile parser file
   let lengths = either (error . show) id parseResult
   let s0 = new [0..255] lengths
-  let (State nsFinal _ _ _) = run s0
+  let (State nsFinal _ _ _) = runRound s0
   putStrLn $ "p1: first 2 ns = " ++ (show (take 2 nsFinal))
   putStrLn $ "p1: product = " ++ (show ((product . take 2) nsFinal))
+  inputStr <- readFile file
+  let h = hash inputStr
+  putStrLn $ "p2 hashed = " ++ h
--- 2017/D10/test/D10Spec.hs
+++ 2017/D10/test/D10Spec.hs
@@ -34,3 +34,38 @@ spec = do
 
       let s4 = step s3
       s4 `shouldBe` State [3, 4, 2, 1, 0] [] 4 4
+
+  describe "seedBytes" $ do
+    it "converts chars to bytes & adds standard suffix" $ do
+      let bs = seedBytes "1,2,3"
+      bs `shouldBe` [49,44,50,44,51,17,31,73,47,23]
+
+    it "gets standard suffix from empty string" $ do
+      let bs = seedBytes ""
+      bs `shouldBe` [17,31,73,47,23]
+
+  describe "xorList" $ do
+    it "xors the list correctly" $ do
+      xorList [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22] `shouldBe` 64
+
+  describe "compactBytes" $ do
+    it "chunks & xors the list" $ do
+      let xs =
+            [ 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22
+            , 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22
+            , 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22
+            ]
+      compactBytes xs `shouldBe` [64, 64, 64]
+
+  describe "hash" $ do
+    it "hashes some known strings" $ do
+      hash "" `shouldBe` "a2582a3a0e66e6e86e3812dcb672a272"
+      hash "AoC 2017" `shouldBe` "33efeb34ea91902bb2f59c9920caa6cd"
+      hash "1,2,3" `shouldBe` "3efbe78a8d82f29979031a4aa0b16a9d"
+      hash "1,2,4" `shouldBe` "63960835bcdc130f0b66d7ff4f6a5a8e"
+
+    it "ignores whitespace" $ do
+      hash "\n" `shouldBe` "a2582a3a0e66e6e86e3812dcb672a272"
+      hash " AoC 2017\n" `shouldBe` "33efeb34ea91902bb2f59c9920caa6cd"
+      hash "1,2,3\n" `shouldBe` "3efbe78a8d82f29979031a4aa0b16a9d"
+      hash "1,2,4\n" `shouldBe` "63960835bcdc130f0b66d7ff4f6a5a8e"
