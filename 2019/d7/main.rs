7213a45defa5418f130ffcf461399644caa8ed9e
Will Fleming <will@flemi.ng>
2019-12-07 11:02:18 -0500

    2019 d7 p1
    I had a really stupid bug in my int machine code from the last day with
    handling input that I guess I was just really lucky didn't impact that
    my answers on day 5. That tied me up for a bit.
    
    Actually that might imply my program for day 5 didn't actually use it's
    input? The bug was that I was grabbing the input's destination index on
    the tape from the wrong index, so it was getting written to the wrong
    spot.

---
+++ 2019/d7/main.rs
@@ -0,0 +1,264 @@
+use std::env::args;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::vec::Vec;
+
+#[derive(Debug)]
+#[derive(Clone)]
+struct IntcodeMachine {
+    tape: Vec<i32>,
+    pos: usize,
+    inputs: Vec<i32>,
+    outputs: Vec<i32>,
+}
+
+fn new(tape: Vec<i32>) -> IntcodeMachine {
+    IntcodeMachine {
+        tape: tape,
+        pos: 0,
+        inputs: Vec::new(),
+        outputs: Vec::new(),
+    }
+}
+
+impl IntcodeMachine {
+    fn push_input(&mut self, v: i32) {
+        self.inputs.push(v);
+    }
+
+    fn run_until_exit(&mut self) {
+        while !self.is_exited() {
+            self.step();
+        };
+    }
+
+    fn step(&mut self) {
+        // println!("DEBUG: step state={:?} opcode={} from tape={}", self, self.cur_opcode(), self.tape[self.pos]);
+        match self.cur_opcode() {
+            1 => self.add(),
+            2 => self.mult(),
+            3 => self.read_input(),
+            4 => self.write_output(),
+            5 => self.jmp_if_true(),
+            6 => self.jmp_if_false(),
+            7 => self.less_than(),
+            8 => self.equals(),
+            99 => (), // halt
+            _ => panic!("bad machine state, invalid opcode from '{}'", self.tape[self.pos]),
+        };
+    }
+
+    fn add(&mut self) {
+        let l = self.nth_param(1);
+        let r = self.nth_param(2);
+        let dest_pos = self.tape[self.pos + 3] as usize;
+        // println!("DEBUG: add {}, {} -> {}", l, r, dest_pos);
+        self.tape[dest_pos] = l + r;
+        self.pos = self.pos + 4;
+    }
+
+    fn mult(&mut self) {
+        let l = self.nth_param(1);
+        let r = self.nth_param(2);
+        let dest_pos = self.tape[self.pos + 3] as usize;
+        // println!("DEBUG: mult {}, {} -> {}", l, r, dest_pos);
+        self.tape[dest_pos] = l * r;
+        self.pos = self.pos + 4;
+    }
+
+    fn read_input(&mut self) {
+        let dest_pos = self.nth_param(1) as usize;
+        // println!("DEBUG: machine reading inputs. input queue={:?}", self.inputs);
+        self.tape[dest_pos] = self.inputs.remove(0);
+        // println!("DEBUG: machine reading inputs. got dest_pos={}, input={}, full tape={:?}", dest_pos, self.tape[dest_pos], self.tape);
+        self.pos += 2;
+    }
+
+    fn write_output(&mut self) {
+        let o = self.nth_param(1);
+        // println!("DEBUG: output {}", o);
+        self.outputs.push(o);
+        self.pos += 2;
+    }
+
+    fn jmp_if_true(&mut self) {
+        if self.nth_param(1) != 0 {
+            self.pos = self.nth_param(2) as usize;
+        } else {
+            self.pos += 3;
+        }
+    }
+
+    fn jmp_if_false(&mut self) {
+        if self.nth_param(1) == 0 {
+            self.pos = self.nth_param(2) as usize;
+        } else {
+            self.pos += 3;
+        }
+    }
+
+    fn less_than(&mut self) {
+        let target: usize = self.tape[self.pos + 3] as usize;
+        if self.nth_param(1) < self.nth_param(2) {
+            self.tape[target] = 1;
+        } else {
+            self.tape[target] = 0;
+        }
+        self.pos += 4;
+    }
+
+    fn equals(&mut self) {
+        let target: usize = self.tape[self.pos + 3] as usize;
+        if self.nth_param(1) == self.nth_param(2) {
+            self.tape[target] = 1;
+        } else {
+            self.tape[target] = 0;
+        }
+        self.pos += 4;
+    }
+
+    fn nth_param(&self, n: u32) -> i32 {
+        self.value_at(
+            self.pos as usize + n as usize,
+            self.nth_param_mode(n),
+        )
+    }
+
+    fn value_at(&self, pos: usize, mode: i32) -> i32 {
+        // println!("DEBUG machine value_at pos={} mode={} tape[pos]={}, v={}", pos, mode, self.tape[pos], self.value(self.tape[pos], mode));
+        self.value(self.tape[pos], mode)
+    }
+
+    fn value(&self, v: i32, mode: i32) -> i32 {
+        match mode {
+            0 => self.tape[v as usize],
+            1 => v,
+            _ => panic!("invalid mode"),
+        }
+    }
+
+    fn is_exited(&self) -> bool {
+          99 == self.cur_opcode()
+    }
+
+    fn nth_param_mode(&self, n: u32) -> i32 {
+        assert!(n >= 1 && n <=3);
+        let div = (10 as i32).pow(n + 1);
+        (self.tape[self.pos] / div) % 10
+    }
+
+    fn cur_opcode(&self) -> i32 {
+        // each digit in the tape entry is (comma sep):
+        // p3mode,p2mode,p1mode,0,opcode
+        self.tape[self.pos] % 100
+    }
+}
+
+fn tape_from_file(path: &str) -> String {
+    let file = File::open(path).expect("couldn't open file");
+    let mut buffered = BufReader::new(file);
+    let mut line = String::new();
+    buffered.read_line(&mut line).expect("no line?");
+
+    line
+}
+
+fn read_tape(line: &str) -> Vec<i32> {
+    line.split(",").map(|x| {
+        let x: i32 = x.trim().parse().expect(&format!("'{}' not an int?", x));
+        x
+    }).collect()
+}
+
+fn input_choices() -> Vec<Vec<i32>> {
+    let mut choices = Vec::new();
+
+    for phase_a in 0..5 {
+        choices.append(&mut build_choices(vec![phase_a]));
+    }
+
+    choices
+}
+
+fn build_choices(phases: Vec<i32>) -> Vec<Vec<i32>> {
+    if phases.len() == 5 {
+        vec![phases]
+    } else {
+        (0..5)
+            .filter(|x| !phases.contains(x))
+            .flat_map(|x| {
+                let mut phases2 = phases.clone();
+                phases2.push(x);
+                build_choices(phases2)
+            }).collect()
+    }
+}
+
+fn thrust_signal(phases: &Vec<i32>, tape: &Vec<i32>) -> i32 {
+    // also could be considered "last amp output"
+    let mut next_amp_input = 0;
+
+    for phase in phases.iter() {
+        let mut machine = new(tape.clone());
+        machine.push_input(*phase);
+        machine.push_input(next_amp_input);
+        machine.run_until_exit();
+
+        // println!("DEBUG: ran machine with phase={}, last_amp={}, output={:?}", phase, next_amp_input, machine.outputs);
+        next_amp_input = *machine.outputs.last().expect("machine should output stuff");
+    }
+
+    // the output of the final amp becomes the input to the thrusters
+    next_amp_input
+}
+
+fn main() {
+    let mut argv = args();
+    argv.next();
+    let input = argv.next().expect("provide a filename");
+    let tape = read_tape(&tape_from_file(&input));
+
+    // p1 - intput 1, run to halt, show outputs
+    let all_phases = input_choices();
+    // println!("DEBUG: all input choices = {:?}", all_phases);
+    let mut best_phases: Option<&Vec<i32>> = None;
+    let mut best_signal = 0;
+    for phases in all_phases.iter() {
+        let s = thrust_signal(&phases, &tape);
+        if s > best_signal {
+            best_phases = Some(&phases);
+            best_signal = s;
+        }
+    }
+    println!("p1: best phases = {:?}, max signal = {}", best_phases, best_signal);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_sample_prog_1_simple() {
+        // I think this program = x + (y * 10)
+        let tape = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
+        let tape = read_tape(&tape);
+
+        let mut machine = new(tape.clone());
+        machine.push_input(1);
+        machine.push_input(2);
+        machine.run_until_exit();
+
+        assert_eq!(21, *machine.outputs.first().expect("should output"));
+    }
+
+    #[test]
+    fn test_sample_prog_1_phases_43210() {
+        let tape = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
+        let tape = read_tape(&tape);
+
+        let signal = thrust_signal(&vec![4, 3, 2, 1, 0], &tape);
+
+        assert_eq!(43210, signal);
+    }
+}
