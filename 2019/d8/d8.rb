52bf6413528d54337f928bfc48dd40be0771cd36
Will Fleming <will@flemi.ng>
2019-12-08 09:40:18 -0500

    2019 d8 p2

--- 2019/d8/d8.rb
+++ 2019/d8/d8.rb
@@ -22,8 +22,42 @@ def p1(layers)
   ones * twos
 end
 
+# turn each layer into nested array, e.g. layer[col][row]
+def layers_to_grids(layers)
+  layers.map do |layer|
+    layer.each_slice(WIDTH).to_a
+  end
+end
+
+def merge_layers(layers)
+  # the last non-transparent pixel wins, keep list reversed
+  layers = layers_to_grids(layers)
+
+  HEIGHT.times.map do |col|
+    WIDTH.times.map do |row|
+      l = layers.find { |l| l[col][row] != 2 }
+      l[col][row]
+    end
+  end
+end
+
+# looks like this image is white-on-black (white text)
+# for readability, print white/1 as *, others as space
+def image_to_s(image)
+  image.map do |row|
+    row.map do |pixel|
+      pixel == 1 ? "*" : " "
+    end.join("")
+  end.join("\n")
+end
+
+def p2(layers)
+  image_to_s(merge_layers(layers))
+end
+
 layers = read_layers(
   File.read(ARGV[0]).strip.split("").map(&:to_i),
 )
 
 puts "p1: #{p1(layers)}"
+puts "p2:\n#{p2(layers)}"
