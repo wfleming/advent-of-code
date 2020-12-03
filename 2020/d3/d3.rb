f41f3c527219cecccdef1215725947cb772c571d
Will Fleming <will@flemi.ng>
2020-12-03 09:04:19 -0500

    2020 d3 p2

--- 2020/d3/d3.rb
+++ 2020/d3/d3.rb
@@ -9,7 +9,6 @@ Point = Struct.new(:x,:y) do
     self.class.new(self.x + other.x, self.y + other.y)
   end
 end
-SLOPE = Point.new(3, 1)
 
 def map_at(pt)
   y_line = MAP[pt.y]
@@ -18,18 +17,31 @@ def map_at(pt)
   y_line[pt.x % y_line.length]
 end
 
-# step by SLOPE until y > max y, return # of trees hit
-def slide(current_pos)
-  tree_cnt = (map_at(current_pos) == TREE ? 1 : 0)
-  next_pos = current_pos + SLOPE
+# step by slope until y > max y, return # of trees hit
+def slide(at:, slope:)
+  tree_cnt = (map_at(at) == TREE ? 1 : 0)
+  next_pos = at + slope
 
   if next_pos.y >= MAP.count
     return tree_cnt
   else
-    return tree_cnt + slide(next_pos)
+    return tree_cnt + slide(at: next_pos, slope: slope)
   end
 end
 
 # p1
-trees = slide(Point.new(0,0))
+trees = slide(at: Point.new(0,0), slope: Point.new(3, 1))
 puts "p1: slid through #{trees} trees"
+
+# p2
+slopes = [
+  Point.new(1, 1),
+  Point.new(3, 1),
+  Point.new(5, 1),
+  Point.new(7, 1),
+  Point.new(1, 2),
+]
+tree_product = slopes.map do |slope|
+  slide(at: Point.new(0, 0), slope: slope)
+end.inject(1, :*)
+puts "p2: product is #{tree_product}"
