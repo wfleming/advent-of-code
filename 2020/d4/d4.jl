03b12371c6052e0c7eebaed181c888c984357428
Will Fleming <will@flemi.ng>
2020-12-04 09:50:08 -0500

    2020 d4 p2

--- 2020/d4/d4.jl
+++ 2020/d4/d4.jl
@@ -11,15 +11,27 @@ REQUIRED_FIELDS = [
   "cid",
 ]
 
+EYE_COLORS = [
+  "amb",
+  "blu",
+  "brn",
+  "gry",
+  "grn",
+  "hzl",
+  "oth",
+]
+
 const Passport = Dict{String,String}
 
 FIELD_PAT = r"(\w+):([#\w]+)[ \n]"
 PASS_SPLIT_PAT = r".*^$"m
+HAIR_PAT = r"^#[\da-f]{6}$"
+PID_PAT = r"^\d{9}$"
 
 function parse_passport(str):: Passport
   passport = Passport()
 
-  # can't match end-of-string in a [] set with " ", and \n causing last field
+  # can't match end-of-string (i.e. \Z) in a [] set causing last field
   # to get dropped. Append a field sep before matching
   str = str * " "
 
@@ -42,8 +54,60 @@ function is_passport_valid(fields, passport):: Bool
   all(f -> f in ks, fields)
 end
 
-# run the logic
+function is_passport_valid_p2(fields, passport):: Bool
+  if !is_passport_valid(fields, passport)
+    return false
+  end
 
+  # now we know the fields are present, we can check the vals
+  byr = parse(Int64, passport["byr"])
+  if !(byr >= 1920 && byr <= 2002)
+    return false
+  end
+
+  iyr = parse(Int64, passport["iyr"])
+  if !(iyr >= 2010 && iyr <= 2020)
+    return false
+  end
+
+  eyr = parse(Int64, passport["eyr"])
+  if !(eyr >= 2020 && eyr <= 2030)
+    return false
+  end
+
+  hgt_parts = match(r"(\d+)(cm|in)", passport["hgt"])
+  if nothing == hgt_parts
+    return false
+  end
+  hgt_n = parse(Int64, hgt_parts[1])
+  if hgt_parts[2] == "cm"
+    if !(hgt_n >= 150 && hgt_n <= 193)
+      return false
+    end
+  elseif hgt_parts[2] == "in"
+    if !(hgt_n >= 59 && hgt_n <= 76)
+      return false
+    end
+  else
+    return false
+  end
+
+  if nothing == match(HAIR_PAT, passport["hcl"])
+    return false
+  end
+
+  if !(passport["ecl"] in EYE_COLORS)
+    return false
+  end
+
+  if nothing == match(PID_PAT, passport["pid"])
+    return false
+  end
+
+  true
+end
+
+# run the logic
 
 # p1
 
@@ -52,3 +116,6 @@ passports = parse_passports(input_str)
 fields_except_cnt = filter(f -> f != "cid", REQUIRED_FIELDS)
 valid_count = count(p -> is_passport_valid(fields_except_cnt, p), passports)
 println("p1: $(valid_count) of $(length(passports)) passports are valid (sans `cid`)")
+
+valid_count_p2 = count(p -> is_passport_valid_p2(fields_except_cnt, p), passports)
+println("p2: $(valid_count_p2) of $(length(passports)) passports are valid (sans `cid`)")
