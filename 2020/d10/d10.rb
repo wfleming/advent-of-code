0bcb9a190b8acc2ff835fe48f13d73c7b9a84b6b
Will Fleming <will@flemi.ng>
2020-12-10 10:05:29 -0500

    2020 d10 p2
    There are much more elegant ways to express a self-memoizing function in
    Ruby, but this works and I've got work to do.

--- 2020/d10/d10.rb
+++ 2020/d10/d10.rb
@@ -1,31 +1,62 @@
 #!/usr/bin/env ruby
 
-def read_adapters
-  File.readlines(ARGV[0]).map { |l| Integer(l) }.sort
+def read_joltages
+  File.readlines(ARGV[0]).map { |l| Integer(l) }.sort.tap do |a|
+    a.unshift(0) # the wall
+    a << a[-1] + 3 # the device
+  end
 end
 
-def joltage_diffs(adapters)
+def joltage_diffs(joltages)
   jumps = Hash.new(0)
 
-  # add the device itself to the adapters
-  adapters = adapters.clone
-  adapters << adapters[-1] + 3
-
-  # at each idx, calculate diff of current & previous
-  adapters.each_index do |idx|
-    # wall is 0, so first one is just its own value
-    diff =
-      if idx == 0
-        adapters[idx]
-      else
-        adapters[idx] - adapters[idx - 1]
-      end
+  # at each idx, calculate diff of next & current
+  (0..(joltages.count - 2)).each do |idx|
+    diff = joltages[idx + 1] - joltages[idx]
     jumps[diff] = jumps[diff] + 1
   end
 
   jumps
 end
 
+# Hash of joltage => immediate predecessor states
+def build_reverse_vertexes(joltages)
+  paths = {}
+
+  joltages_r = joltages.reverse
+  joltages_r.each_index do |i|
+    next if i == joltages_r.count - 1
+
+    dest = joltages_r[i]
+    pred_states = joltages_r.drop(i + 1).take(3).select { |x| x >= dest - 3 }
+
+    paths[dest] = pred_states
+  end
+
+  paths
+end
+
+def count_paths_to(x, reverse_vs)
+  # the wall is the base case
+  return 1 if x == 0
+
+  $path_count_cache ||= {} # map x => the known answer
+  return $path_count_cache[x] if $path_count_cache.key?(x)
+
+  preds = reverse_vs[x]
+
+  preds.map { |p| count_paths_to(p, reverse_vs) }.sum.tap do |ans|
+    $path_count_cache[x] ||= ans
+  end
+end
+
+joltages = read_joltages.freeze
+
 # p1
-diffs = joltage_diffs(read_adapters)
+diffs = joltage_diffs(joltages)
 puts "p1: diff counts: #{diffs}. answer is #{diffs[1] * diffs[3]}"
+
+# p2
+goal = joltages[-1]
+reverse_vertexes = build_reverse_vertexes(joltages)
+puts "p2: there are #{count_paths_to(goal, reverse_vertexes)} paths to #{goal}"
