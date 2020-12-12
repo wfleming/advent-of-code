87b8e54377277d930886bfa1a946001ae9c57493
Will Fleming <will@flemi.ng>
2020-12-12 09:39:57 -0500

    2020 d12 p2

--- 2020/d12/d12.rb
+++ 2020/d12/d12.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+INSTRUCTION_PAT = /^(?<action>[NSWERLF])(?<amount>\d+)/
+
 Point = Struct.new(:x, :y) do
   def distance(other)
     (other.x - x).abs + (other.y - y).abs
@@ -9,13 +11,31 @@ Point = Struct.new(:x, :y) do
     self.class.new(x + other.x, y + other.y)
   end
 
+  def -(other)
+    self.class.new(x - other.x, y - other.y)
+  end
+
   def *(scale)
     self.class.new(x * scale, y * scale)
   end
 end
 
+def vec_from_dir(dir)
+  case dir
+  when "N"
+    Point.new(0, 1)
+  when "S"
+    Point.new(0, -1)
+  when "W"
+    Point.new(-1, 0)
+  when "E"
+    Point.new(1, 0)
+  else
+    raise ArgumentError, "invalid direction #{dir}"
+  end
+end
+
 Ship = Struct.new(:heading, :position) do
-  INSTRUCTION_PAT = /^(?<action>[NSWERLF])(?<amount>\d+)/
   ROT_DIRS = ["N", "E", "S", "W"].freeze # in the order of clockwise rotation
 
   def navigate_all!(lines)
@@ -26,30 +46,15 @@ Ship = Struct.new(:heading, :position) do
     m = INSTRUCTION_PAT.match(line)
     case m["action"]
     when "N", "S", "W", "E"
-      self.position += direction_vec(m["action"]) * Integer(m["amount"])
+      self.position += vec_from_dir(m["action"]) * Integer(m["amount"])
     when "F"
-      self.position += direction_vec(heading) * Integer(m["amount"])
+      self.position += vec_from_dir(heading) * Integer(m["amount"])
     when "R"
       self.heading = rotate(heading, Integer(m["amount"]))
     when "L"
       self.heading = rotate(heading, 0 - Integer(m["amount"]))
     else
       raise ArgumentError, "invalid action in #{line}"
-    end
-  end
-
-  def direction_vec(dir)
-    case dir
-    when "N"
-      Point.new(0, 1)
-    when "S"
-      Point.new(0, -1)
-    when "W"
-      Point.new(-1, 0)
-    when "E"
-      Point.new(1, 0)
-    else
-      raise ArgumentError, "invalid direction #{dir}"
     end
   end
 
@@ -62,8 +67,53 @@ Ship = Struct.new(:heading, :position) do
   end
 end
 
+Ship2 = Struct.new(:position, :waypoint) do
+  def navigate_all!(lines)
+    lines.each(&method(:navigate_step!))
+  end
+
+  def navigate_step!(line)
+    m = INSTRUCTION_PAT.match(line)
+    case m["action"]
+    when "N", "S", "W", "E"
+      self.waypoint += vec_from_dir(m["action"]) * Integer(m["amount"])
+    when "F"
+      self.position += waypoint * Integer(m["amount"])
+    when "R"
+      self.waypoint = rotate(waypoint, 360 - Integer(m["amount"]))
+    when "L"
+      self.waypoint = rotate(waypoint, Integer(m["amount"]))
+    else
+      raise ArgumentError, "invalid action in #{line}"
+    end
+  end
+
+  # waypoint is always treated relative to position - it's a vector, not an
+  # absolute position. So we can just rotate it relative to (0,0), not the ship's
+  # real position.
+  # And traditionally angles sweep counter-clockwise in math, I always forget
+  # that.
+  def rotate(point, degrees)
+    raise ArgumentError, "can only turn 90 degree increments" unless (degrees % 90).zero?
+    raise ArgumentError, "can only turn 90 - 270" if degrees > 270 || degrees <= 0
+
+    radians = degrees * Math::PI / 180
+    sin = Math.sin(radians)
+    cos = Math.cos(radians)
+    Point.new(
+      (point.x * cos - point.y * sin).round,
+      (point.x * sin + point.y * cos).round,
+    )
+  end
+end
+
 # p1
 origin = Point.new(0, 0)
 s = Ship.new("E", origin)
 s.navigate_all!(File.readlines(ARGV[0]))
 puts "p1: ship ended up at #{s.position}, distance=#{origin.distance(s.position)}"
+
+# p2
+s = Ship2.new(origin, Point.new(10, 1))
+s.navigate_all!(File.readlines(ARGV[0]))
+puts "p2: ship ended up at #{s.position}, distance=#{origin.distance(s.position)}"
