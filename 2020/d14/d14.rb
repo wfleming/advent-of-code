0b9cd9b2990a25d39af208c72dce73de1b62575e
Will Fleming <will@flemi.ng>
2020-12-14 10:12:43 -0500

    2020 d14 p2
    This seems like an unusual AOC case where the *example* is not
    computable with a naive approach, but the *input* (at least my input)
    is.
    
    When the mask has lots of wildcards ("X"), fully expanding it is a very
    bad idea. But the masks in my input all had a pretty low number of
    wildcards (the highest was 8, for 256 distinct expanded values). The p1
    example program had 34 wildcards (17 billion expansions), although the
    p2 examples were more reasonable.
    
    I think the gold-plated solution here is to not actually expand the
    wildcards, but to remember the stored values *with* the wildcard
    addresses, and then count out how many expansions there would be at the
    end & multiply the values. That's more challenging, though, because
    later addresses can intersect with earlier addresses (although maybe in
    the given inputs, they don't? I didn't check that), so I think you'd
    want to end up storing it as some kind of tree so you track the ordering
    and which particular expansions of a given address were overridden
    later.  That's an interesting problem to consider, but it's Monday
    morning & I have actual work to do, so I'm taking my gold stars where I
    can get them.

--- 2020/d14/d14.rb
+++ 2020/d14/d14.rb
@@ -17,21 +17,22 @@ class Program
     memory.values.sum
   end
 
-  def run_program(instructions)
-    instructions.each(&method(:run_instruction))
+  def run_program(instructions, version: 1)
+    method = "run_instruction_v#{version}".to_sym
+    instructions.each(&method(method))
   end
 
-  def run_instruction(instruction)
+  def run_instruction_v1(instruction)
     if (m = MASK_PAT.match(instruction))
       @mask = m["mask"]
     elsif (m = MEM_PAT.match(instruction))
-      memory[Integer(m["addr"])] = apply_mask(Integer(m["val"]))
+      memory[Integer(m["addr"])] = apply_mask_val(Integer(m["val"]))
     else
       raise "couldn't parse '#{instruction}'"
     end
   end
 
-  def apply_mask(n)
+  def apply_mask_val(n)
     raise "should have a mask set before setting mem" if mask.nil?
     n_bin = "%0#{mask.length}b" % n
 
@@ -43,9 +44,53 @@ class Program
 
     Integer(n_bin, 2)
   end
+
+  def run_instruction_v2(instruction)
+    if (m = MASK_PAT.match(instruction))
+      @mask = m["mask"]
+    elsif (m = MEM_PAT.match(instruction))
+      # puts "DEBUG: v2 mapped addr #{m["addr"]} with mask #{mask} to #{apply_mask_addr(m["addr"])}"
+      apply_mask_addr(m["addr"]).each do |addr|
+        memory[addr] = Integer(m["val"])
+      end
+    else
+      raise "couldn't parse '#{instruction}'"
+    end
+  end
+
+  def apply_mask_addr(n)
+    raise "should have a mask set before setting mem" if mask.nil?
+    n_bin = "%0#{mask.length}b" % n
+
+    # flip the bits first
+    mask.each_char.each_with_index do |char, idx|
+      n_bin[idx] = char if %w[1 X].include?(char)
+    end
+
+    # expand the Xs
+    expand_masked_addr(n_bin).map(&method(:Integer))
+  end
+
+  # I know this is a bad idea generally, see commit message
+  def expand_masked_addr(str)
+    return [""] if str.length == 0
+    h, t = str[0], str[1..]
+
+    # puts "DEBUG: expand_addr h=#{h} t=#{t}"
+    if h == "X"
+      expand_masked_addr(t).flat_map { |t2| ["1#{t2}", "0#{t2}"] }
+    else
+      expand_masked_addr(t).map { |t2| "#{h}#{t2}" }
+    end
+  end
 end
 
 # p1
 p = Program.new
 p.run_program(INSTRUCTIONS)
 puts "p1: sum of memory is #{p.mem_sum}"
+
+# p2
+p2 = Program.new
+p2.run_program(INSTRUCTIONS, version: 2)
+puts "p2: sum of memory is #{p2.mem_sum}"
