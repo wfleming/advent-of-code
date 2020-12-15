50272d53fddb8d838eb9f9601427777fc8d1ed80
Will Fleming <will@flemi.ng>
2020-12-15 08:56:44 -0500

    2020 d15 p1

---
+++ 2020/d15/d15.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+SEED = File.read(ARGV[0]).split(",").map(&method(:Integer))
+
+class Game
+  attr_reader :seed, :ages, :turn, :last_said
+
+  def initialize(seed)
+    @seed = seed
+    @ages = {} # number spoken => last 2 turns it was spoken
+    @last_said = nil
+    @turn = 0
+  end
+
+  def play_turn!
+    n =
+      if turn >= seed.count
+        if ages[last_said].count == 1
+          0
+        else
+          ages[last_said][1] - ages[last_said][0]
+        end
+      else # still in the early seed ns
+        seed[turn]
+      end
+    @turn += 1
+    ages[n] ||= []
+    ages[n] << turn
+    ages[n] = ages[n][-2..] if ages[n].count > 2
+    @last_said = n
+    # puts "on turn #{turn}, #{n} was spoken"
+  end
+
+  def play_turns!(turn_count)
+    turn_count.times { play_turn! }
+  end
+end
+
+# p1
+g = Game.new(SEED)
+g.play_turns!(2020)
+puts "p1: after 2020 turns, last number spoken was #{g.last_said}"
