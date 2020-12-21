11dfe693974b14d49db195f5324ae84e0dfe0a2f
Will Fleming <will@flemi.ng>
2020-12-21 09:43:08 -0500

    2020 d21 p2

--- 2020/d21/d21.rb
+++ 2020/d21/d21.rb
@@ -50,14 +50,49 @@ class FoodList
   def non_allergenic_ingredients
     @non_allergenic_ingredients = all_ingredients - possible_allergen_ingredients.values.flatten
   end
+
+  # narrowed down to hash of allergen => ingredient
+  def allergen_ingredients
+    @allergen_ingredients ||=
+      begin
+        h = Hash[ possible_allergen_ingredients.map { |k, v| [k, v.clone] } ]
+
+        while h.any? { |_, ingredients| ingredients.count > 1 }
+          unique_ingredients = h.select { |_, ingredients|
+            ingredients.count == 1
+          }.values.flatten
+
+          h = Hash[
+            h.map do |allergen, ingredients|
+              new_ingredients =
+                if ingredients.count == 1
+                  ingredients
+                else
+                  ingredients.reject { |i| unique_ingredients.include?(i) }
+                end
+              [allergen, new_ingredients]
+            end
+          ]
+        end
+
+        # now the ingredients are all 1-element arrays, unwrap them
+        Hash[
+          h.map { |allergen, ingredients| [allergen, ingredients[0]] }
+        ]
+      end
+  end
 end
 
 list = FoodList.new(File.read(ARGV[0]))
 
-# puts "DEBUG:
 # p1 - count occurences of non allergenic ingredients
 occurences = list.non_allergenic_ingredients.map do |ingredient|
   list.foods.count { |food| food.ingredients.include?(ingredient) }
 end.sum
-puts "p1: possible_allergen_ingredients=#{list.possible_allergen_ingredients}"
 puts "p1: there are #{occurences} occurences of those ingredients"
+
+allergen_ingredients_alphabetical = list.allergen_ingredients.
+  sort_by { |allergen, ingredient| allergen }.
+  map { |pair| pair[1] }.
+  join(",")
+puts "p2: #{allergen_ingredients_alphabetical}"
