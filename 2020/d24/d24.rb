c9cac2d50c1772752b21c121465beb499d3de871
Will Fleming <will@flemi.ng>
2020-12-24 09:10:13 -0500

    2020 d24 p2

--- 2020/d24/d24.rb
+++ 2020/d24/d24.rb
@@ -29,6 +29,10 @@ class Grid
     def southwest
       self.class.new(x - 1, y, z + 1)
     end
+
+    def neighbors
+      [ east, west, northeast, northwest, southeast, southwest ]
+    end
   end
 
   attr_reader :black_tiles
@@ -76,6 +80,26 @@ class Grid
       black_tiles.add(coord)
     end
   end
+
+  def cycle
+    coords_to_flip = black_tiles.flat_map do |black_coord|
+      black_neighbor_cnt = black_coord.neighbors.count { |n| black_tiles.include?(n) }
+      flip_self = black_neighbor_cnt == 0 || black_neighbor_cnt > 2
+
+      white_neighbors = black_coord.neighbors.reject { |n| black_tiles.include?(n) }
+      white_neighbors_to_flip = white_neighbors.select { |white_coord|
+        white_coord.neighbors.count { |n| black_tiles.include?(n) } == 2
+      }
+
+      (flip_self ? [black_coord] : []) + white_neighbors_to_flip
+    end
+
+    coords_to_flip.uniq.each(&method(:flip_tile))
+  end
+
+  def cycle_n(n)
+    n.times { cycle }
+  end
 end
 
 grid = Grid.new
@@ -83,4 +107,8 @@ File.open(ARGV[0]).each_line do |line|
   dest = grid.walk_to_tile(line)
   grid.flip_tile(dest)
 end
-puts "p1: there are #{grid.black_tiles.count} black tiles"
+puts "p1: (day 0) there are #{grid.black_tiles.count} black tiles"
+
+# p2 applies to the result of p1 - the p1 directions are the seed
+grid.cycle_n(100)
+puts "p2: after 100 days there are #{grid.black_tiles.count} black tiles"
