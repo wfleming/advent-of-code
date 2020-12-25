49412c98303de2ad83e25ca03d6480ff25559be1
Will Fleming <will@flemi.ng>
2020-12-25 09:12:27 -0500

    2020 d25 p1
    and p2 is a freebie, so we're done!

---
+++ 2020/d25/d25.jl
@@ -0,0 +1,56 @@
+#!/usr/bin/env julia
+
+function read_pub_keys()
+  map(Base.Fix1(parse, Int64), eachline(ARGS[1]))
+end
+
+MODULUS = 20201227
+
+function transform_once(subject, current_val)
+  (current_val * subject) % MODULUS
+end
+
+function transform(subject, loops)
+  val = subject
+
+  # again, not sure why this is off-by-1 and needs to loop *fewer* times
+  for _ in 1:(loops - 1)
+    val = transform_once(subject, val)
+  end
+
+  val
+end
+
+INITIAL_SUBJECT = 7
+
+function find_loop_sizes(pub_key)
+  loop_count = 1
+  val = transform_once(INITIAL_SUBJECT, INITIAL_SUBJECT)
+
+  while val != pub_key
+    loop_count += 1
+    val = transform_once(INITIAL_SUBJECT, val)
+
+    if loop_count > 100_000_000
+      error("that loop count seems much too high")
+    end
+  end
+
+  # I confess I don't understand why this was off-by-1
+  loop_count + 1
+end
+
+pub_keys = read_pub_keys()
+
+loop_sizes = map(find_loop_sizes, pub_keys)
+
+enc_keys = map(
+  (x) -> transform(x[1], x[2]),
+  zip(pub_keys, reverse(loop_sizes))
+)
+
+if enc_keys[1] != enc_keys[2]
+  error("both sides should get the same encryption key")
+end
+
+println("p1: encryption key is $(enc_keys[1])")
