f2894e1ba34a6c03c8bfe887940d21e787584005
Will Fleming <will@flemi.ng>
2021-11-15 14:15:13 -0500

    2015 d19 p2
    This probably wasn't the fastest way for me to get to an answer. Some
    kind of search was my first instinct, and when it became very clear that
    A* searching *forwards* from e -> goal was completely infeasible, I did
    some reasearch of other people's solutions and found that searching
    backwards was probably faster. So I ended up doing that, and it was ok
    with the first cut that did a pretty naive "priority queue" with an
    array and a call to `sort_by!` after each loop - about 25s. That would
    have been good enough, but I decided to have some fun and see how much a
    proper priority queue helped - the basic queue implementation didn't
    help much at all because checking membership was expensive. Adding a
    set for membership checks to the queue got this down to 7s, which is
    good enough for me. I saw some other solutions based on random sampling
    that probably would have run faster and been less code, but wouldn't
    have been as semantically "correct" or elegant.

--- 2015/d19/d19.rb
+++ 2015/d19/d19.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require "set"
+
 def parse_input
   lines = File.readlines(ARGV[0])
 
@@ -31,7 +33,145 @@ def available_transforms(molecule, transforms)
   end.uniq
 end
 
+# https://stackoverflow.com/questions/46402903/levenshtein-distance-in-ruby
+def levenshtein_distance(s, t)
+  v0 = (0..t.length).to_a
+  v1 = []
+
+  s.chars.each_with_index do |s_ch, i|
+    v1[0] = i + 1
+
+    t.chars.each_with_index do |t_ch, j|
+      cost = s_ch == t_ch ? 0 : 1
+      v1[j + 1] = [v1[j] + 1, v0[j + 1] + 1, v0[j] + cost].min
+    end
+    v0 = v1.dup
+  end
+
+  v0[t.length]
+end
+
+# https://www.brianstorti.com/implementing-a-priority-queue-in-ruby/
+class PQueue
+  def initialize()
+    # each element is [actual_element, priority], root remains nil
+    @storage = [nil]
+    @includes = Set.new
+  end
+
+  def any?
+    count > 0
+  end
+
+  def count
+    # don't count nil root
+    @storage.count - 1
+  end
+
+  def include?(x)
+    @includes.include?(x)
+  end
+
+  # only returns the element, not the stored priority
+  def shift
+    swap(1, @storage.size - 1)
+    head = @storage.pop # head/highest priority is now at end of storage
+    bubble_down(1) # make sure the tree is ordered again
+    @includes.delete(head[0])
+    head[0]
+  end
+
+  def push(el, priority)
+    @storage << [el, priority]
+    @includes << el
+    bubble_up(@storage.count - 1)
+  end
+
+  def bubble_up(idx)
+    parent_idx = idx / 2
+    return if parent_idx < 1
+    return if @storage[parent_idx][1] <= @storage[idx][1]
+    swap(idx, parent_idx)
+    bubble_up(parent_idx)
+  end
+
+  def bubble_down(idx)
+    child_index = idx * 2
+    return if child_index > @storage.count - 1
+    not_last_el = child_index < @storage.count - 1
+    left_child = @storage[child_index]
+    right_child = @storage[child_index + 1]
+    child_index += 1 if not_last_el && right_child[1] < left_child[1]
+    return if @storage[idx][1] <= @storage[child_index][1]
+    swap(idx, child_index)
+    bubble_down(child_index)
+  end
+
+  def swap(idx1, idx2)
+    @storage[idx1], @storage[idx2] = @storage[idx2], @storage[idx1]
+  end
+end
+
+# https://en.wikipedia.org/wiki/A*_search_algorithm
+def astar_search(start, goal, transforms)
+  open_set = PQueue.new()
+  open_set.push(start, levenshtein_distance(start, goal))
+
+  came_from = {} # molecule => prev_molecule
+  g_scores = {start => 0} # molecule => # transforms to get there
+  f_scores = {start => levenshtein_distance(start, goal)}
+
+  # i = 0
+  # loop_times, sort_times = [], []
+  while open_set.any?
+    n = open_set.shift
+    # if i % 100 == 0 # DEBUG
+    #   puts "DEBUG: avg loop time = #{loop_times.sum / loop_times.count} avg sort time = #{sort_times.sum / sort_times.count }" if loop_times.count > 0
+    #   puts "DEBUG: current estimated distance to goal = #{f_scores[n]}, open_set.count=#{open_set.count}"
+    #   loop_times, sort_times = [], []
+    # end
+    # t = Process.clock_gettime(Process::CLOCK_MONOTONIC) # DEBUG
+    # i += 1 # DEBUG
+    if n == goal
+      path = [n]
+      while came_from.include?(n)
+        n = came_from[n]
+        path.unshift(n)
+      end
+      return path
+    end
+    available_transforms(n, transforms).each do |next_molecule|
+      g_score = g_scores[n] + 1
+      if g_score < g_scores.fetch(next_molecule, Float::INFINITY)
+        came_from[next_molecule] = n
+        g_scores[next_molecule] = g_score
+        f_scores[next_molecule] = g_score + levenshtein_distance(next_molecule, goal)
+        open_set.push(next_molecule, f_scores[next_molecule]) unless open_set.include?(next_molecule)
+      end
+    end
+    # st = Process.clock_gettime(Process::CLOCK_MONOTONIC) # DEBUG
+    # open_set.sort_by! { |s| f_scores[s] }
+    # et = Process.clock_gettime(Process::CLOCK_MONOTONIC) # DEBUG
+    # loop_times << et - t # DEBUG
+    # sort_times << et - st # DEBUG
+  end
+
+  raise StandardError, "Could not find path to goal"
+end
+
 medicine_molecule, transforms = *parse_input
 
 potential_next_molecules = available_transforms(medicine_molecule, transforms)
 puts "p1: #{potential_next_molecules.count} potential molecules can be created with 1 transform"
+
+# backwards search is considerably more efficient
+inverted_transforms = transforms.inject({}) do |memo, replacement_pair|
+  replacement_source, replacements = replacement_pair
+  replacements.each do |replacement|
+    memo[replacement] ||= []
+    memo[replacement] << replacement_source
+  end
+  memo
+end
+p2_steps = astar_search(medicine_molecule, "e", inverted_transforms)
+puts "p2: #{p2_steps.count - 1} is the minimum number of transforms"
