9acc129f4a3dfc7334657ab13fd9a39363f7a5bd
Will Fleming <will@flemi.ng>
2021-12-10 09:24:34 -0500

    2021 d10 - DRY up part 2

--- 2021/d10/d10.rb
+++ 2021/d10/d10.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
 Error = Struct.new(:found, :expected)
+ErrorResult = Struct.new(:errors, :open_stack)
 
 PAIRS = { "(" => ")", "{" => "}", "[" => "]", "<" => ">"}
 ERR_SCORES = { ")" => 3, "]" => 57, "}" => 1197, ">" => 25137 }
 COMPLETION_SCORES = { ")" => 1, "]" => 2, "}" => 3, ">" => 4 }
 
+# return { errors: [], open_stack: [] }
 def find_errors(line)
   open_stack = []
   errors = []
@@ -21,24 +23,16 @@ def find_errors(line)
     end
   end
 
-  errors
+  ErrorResult.new(errors, open_stack)
 end
 
 # return nil if line is corrupt, array of chars otherwise
 def auto_complete(line)
-  open_stack = []
+  errs_and_open_stack = find_errors(line)
 
-  line.each_char do |char|
-    if PAIRS.include?(char) # new open
-      open_stack.push(char)
-    elsif char != PAIRS.fetch(open_stack[-1]) # invalid close
-      return nil
-    else # valid close
-      open_stack.pop
-    end
-  end
+  return nil if errs_and_open_stack.errors.any?
 
-  open_stack.reverse.map { |c| PAIRS.fetch(c) }
+  errs_and_open_stack.open_stack.reverse.map { |c| PAIRS.fetch(c) }
 end
 
 def score_completion(completions)
@@ -46,7 +40,7 @@ def score_completion(completions)
 end
 
 lines = File.readlines(ARGV[0]).map(&:chomp)
-first_errs = lines.map(&method(:find_errors)).select(&:any?).map(&:first)
+first_errs = lines.map(&method(:find_errors)).map(&:errors).select(&:any?).map(&:first)
 score = first_errs.sum { |e| ERR_SCORES.fetch(e.found) }
 puts "p1: score = #{score}"
 
