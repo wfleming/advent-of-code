6f56d61b33e47123d5e2648c583f86872e74d192
Will Fleming <will@flemi.ng>
2021-12-20 10:59:42 -0500

    2021 d20 faster
    Ok, maybe I was going to fiddle with this a bit more. After I wrote down
    the perf change I *wasn't* going to do in the previous commit message,
    I decided implementing it would actually be pretty quick, so I did it.
    
    It definitely helps. Runtime for both parts dropped to 27s, a 38%
    improvement. Still not very fast, but much better.
    
    Recorded a run with rbspy out of curiosity - looks like the bottleneck
    is the "set.include?" checks. I tried some different `Point#hash`
    implementations to see if it was a case of bad hash bucket collisions -
    a few approaches shaved another couple of seconds off, but nothing very
    significant. Changing hash implementations in ruby is a bit of a
    low yield optimization most of the time anyway: the default
    implementation is a native C function, so the slowdown from a bad hash
    has to be pretty bad in order to get a win from replacing that C
    function with a ruby function.
    
    The better approach would, I think, just be replacing the set of lit
    pixels with a proper matrix/nested arrays. I'd probably want to
    pre-allocate them to be large enough for all the pixels I'd want to
    track after x rounds of enhancement so I wouldn't need to resize/remap
    coordinates after each iteration or anything. Don't think I'm going to
    bother trying that, though. I mean it this time.

--- 2021/d20/d20.rb
+++ 2021/d20/d20.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require "set"
+
 Point = Struct.new(:x, :y) do
   def surrounding
     [
@@ -23,53 +25,42 @@ class Image
     enhancement_algorithm = lines.shift
     lines.shift # blank line
 
-    # at first I thought I'd just track the the lit pixels in a set, but I'm
-    # concerned that could lead to accidentally considering some pixels part of
-    # the "outer set" when they shouldn't be and getting the wrong value, so
-    # I'll just track a hash. the set of "center pixels I need to care about"
-    # won't grow that fast (for p1, here's hoping for p2)
-    pixels = Hash.new
+    lit_pixels = Set.new
     lines.each_with_index do |l, y|
       l.each_char.with_index do |c, x|
-        pixels[Point.new(x, y)] = (c == "#")
+        lit_pixels << Point.new(x, y) if c == "#"
       end
     end
 
-    new(enhancement_algorithm, pixels)
+    new(enhancement_algorithm, lit_pixels)
   end
 
-  attr_reader :pixels, :outer_pixels_lit
+  attr_reader :lit_pixels, :outer_pixels_lit, :x_range, :y_range
 
-  def initialize(enhancement_algorithm, pixels)
+  def initialize(enhancement_algorithm, lit_pixels)
     @enhancement_algorithm = enhancement_algorithm
-    @pixels = pixels
-    @outer_pixels_lit = false
-  end
-
-  def x_range
-    @x_range ||= begin
-      r = pixels.minmax_by { |k, _| k.x }.map { |k, _| k.x }
+    @lit_pixels = lit_pixels
+    @x_range = begin
+      r = lit_pixels.minmax_by(&:x).map(&:x)
       r[0]..r[1]
-   end
-  end
-
-  def y_range
+    end
     @y_range ||= begin
-      r = pixels.minmax_by { |k, _| k.y }.map { |k, _| k.y }
+      r = lit_pixels.minmax_by(&:y).map(&:y)
       r[0]..r[1]
-   end
+    end
+    @outer_pixels_lit = false
   end
 
   def char_at(pt)
     if x_range.include?(pt.x) && y_range.include?(pt.y)
-      pixels[pt] ? "#" : "."
+      lit_pixels.include?(pt) ? "#" : "."
     else
       outer_pixels_lit ? "#" : "."
     end
   end
 
   def enhance!
-    new_pixels = pixels.clone
+    new_lit_pixels = lit_pixels.clone
 
     # to properly account for the "border" changing, we widen the x/y range
     ((y_range.first - 1)..(y_range.last + 1)).each do |y|
@@ -80,20 +71,22 @@ class Image
 
         # puts "enhance (#{x},#{y}): was #{char_at(Point.new(x,y))}, surrounding = #{surrounding_pat}, becomes #{new_char}"
 
-        new_pixels[Point.new(x,y)] = (new_char == "#")
+        if new_char == "#"
+          new_lit_pixels << Point.new(x,y)
+        else
+          new_lit_pixels.delete(Point.new(x,y))
+        end
       end
     end
-    @pixels = new_pixels
+    @lit_pixels = new_lit_pixels
+    @y_range = ((y_range.first - 1)..(y_range.last + 1))
+    @x_range = ((x_range.first - 1)..(x_range.last + 1))
 
     # example input doesn't change outer pixels, but actual input swaps them all
     # each iteration, and the image is infinite, so...
     outer_pixels_pat = (outer_pixels_lit ? "1" : "0") * 9
     outer_pixels_enhancement_idx = outer_pixels_pat.to_i(2)
     @outer_pixels_lit = (@enhancement_algorithm[outer_pixels_enhancement_idx] == "#")
-
-    # x, y ranges are cached for perf, but may have changes, so nil them out
-    @x_range = nil
-    @y_range = nil
 
     self
   end
@@ -110,8 +103,8 @@ end
 if __FILE__ == $0
   img = Image.parse(File.readlines(ARGV[0]))
   img.enhance!.enhance!
-  puts "p1: image has #{img.pixels.count { |_, v| v }} lit pixels"
+  puts "p1: image has #{img.lit_pixels.count} lit pixels"
 
   48.times { img.enhance! }
-  puts "p2: image has #{img.pixels.count { |_, v| v }} lit pixels"
+  puts "p2: image has #{img.lit_pixels.count} lit pixels"
 end
