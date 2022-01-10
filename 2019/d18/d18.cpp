aa0cbb74c0d3ce313cf28d68c8cc607ec88825cc
Will Fleming <will@flemi.ng>
2022-01-09 21:48:10 -0500

    astar impl, other stuff

--- 2019/d18/d18.cpp
+++ 2019/d18/d18.cpp
@@ -1,16 +1,21 @@
-#include <iostream>
+#include <algorithm>
 #include <fstream>
+#include <iostream>
+#include <memory>
+#include <queue>
+#include <set>
 #include <sstream>
-#include <unordered_set>
 #include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
 
 class Point {
   public:
     int x, y;
+    Point(): x{0}, y{0} {}; // needed for unordered_map[]=
     Point(int x, int y): x{x}, y{y} {};
-    ~Point() = default;
 
     operator std::string() const {
       ostringstream s;
@@ -24,6 +29,10 @@ class Point {
 
     bool operator!=(const Point& other) const {
       return !(*this == other);
+    }
+
+    unsigned int distance(const Point& other) const {
+      return abs(x - other.x) + abs(y - other.y);
     }
 
     vector<Point> neighbors() {
@@ -49,32 +58,33 @@ template<> struct std::hash<Point> {
 
 class Maze {
   public:
-    unordered_set<Point> floor;
-    unordered_map<char, Point> doors;
-    unordered_map<char, Point> keys;
+    shared_ptr<unordered_set<Point>> floor;
+    shared_ptr<unordered_map<Point, char>> doors;
+    shared_ptr<unordered_map<Point, char>> keys;
     Point cur_pos;
-    unordered_set<char> current_keys;
+    char goal_key;
+    set<char> held_keys;
 
     static Maze parse(istream& in) {
       Maze maze = Maze();
       unsigned int y = 0;
       string line;
-      while(in.good()) {
+      while (in.good()) {
         getline(in, line);
-        for(unsigned int x = 0; x < line.size(); x++) {
+        for (unsigned int x = 0; x < line.size(); x++) {
           char c = line[x];
           Point p = Point(x,y);
           if (c == '.') {
-            maze.floor.insert(p);
+            maze.floor->insert(p);
           } else if (c == '@') {
-            maze.floor.insert(p);
+            maze.floor->insert(p);
             maze.cur_pos = p;
           } else if (isupper(c)) {
-            maze.floor.insert(p);
-            /* maze.doors[c] = p; */
+            maze.floor->insert(p);
+            (*maze.doors)[p] = c;
           } else if (islower(c)) {
-            maze.floor.insert(p);
-            /* maze.keys[c] = p; */
+            maze.floor->insert(p);
+            (*maze.keys)[p] = c;
           }
         }
         y++;
@@ -83,32 +93,157 @@ class Maze {
     }
 
     Maze():
-      floor{unordered_set<Point>()},
-      doors{unordered_map<char, Point>()},
-      keys{unordered_map<char, Point>()},
+      // is there a more concise way to say this? I'm surprised I can't omit the
+      // types and do shared_ptr<>{...}
+      floor{shared_ptr<unordered_set<Point>>{new unordered_set<Point>()}},
+      doors{shared_ptr<unordered_map<Point, char>>{new unordered_map<Point, char>()}},
+      keys{shared_ptr<unordered_map<Point, char>>{new unordered_map<Point, char>()}},
       cur_pos{Point(0,0)},
-      current_keys{unordered_set<char>()}
-      {};
+      goal_key{'_'},
+      held_keys{set<char>()}
+      { };
+
+    // similar to hash below, we only really need to check cur_pos & held_keys
+    bool operator==(const Maze& other) const {
+      return other.cur_pos == cur_pos && other.held_keys == held_keys;
+    }
+
+    bool operator!=(const Maze& other) const {
+      return !(*this == other);
+    }
+
+    vector<char> available_keys() {
+      vector<char> rv;
+      for (auto p : *keys) {
+        if (!held_keys.contains(p.second)) {
+          rv.push_back(p.second);
+        }
+      }
+      return rv;
+    }
 
     bool can_open(char door) {
-      return current_keys.contains(tolower(door));
+      return held_keys.contains(tolower(door));
+    }
+
+    bool can_step(const Point& dest) {
+      if (doors->contains(dest)) {
+        return can_open((*doors)[dest]);
+      } else {
+        return floor->contains(dest);
+      }
+    }
+
+    Maze step_to(Point dest) {
+      Maze maze = Maze();
+      maze.floor = floor;
+      maze.doors = doors;
+      maze.keys = keys;
+      maze.held_keys = held_keys;
+      if (keys->contains(dest) && !held_keys.contains((*keys)[dest])) {
+        maze.held_keys.insert((*keys)[dest]);
+      }
+      maze.cur_pos = dest;
+      return maze;
     }
 
     bool is_goal() {
-      for(auto it=keys.begin(); it!=keys.end(); it++) {
-        if (!current_keys.contains(it->first)) { return false; }
+      for (auto p : *keys) {
+        if (!held_keys.contains(p.second)) { return false; }
       }
       return true;
     }
-    // TODO: subsequent states. Can move through doors if have key.
+
+    unsigned int f_score() {
+      // use the total manhattan distance to every key we *don't* have.
+      int f = 0;
+      for (auto p : *keys) {
+        if (!held_keys.contains(p.second)) {
+          f += cur_pos.distance(p.first);
+        }
+      }
+      return f;
+    }
+
+    vector<Maze> next_states() {
+      vector<Maze> rv;
+      for (auto n : cur_pos.neighbors()) {
+        if (can_step(n)) {
+          rv.push_back(step_to(n));
+        }
+      }
+      return rv;
+    }
 };
 
-// TODO - search a path. A*?
+template<> struct std::hash<Maze> {
+  size_t operator()(const Maze& m) const noexcept {
+    // we can safely omit floor, door, keys since they're the same for every
+    // instance we'll be dealing with.
+    size_t h = std::hash<Point>{}(m.cur_pos);
+    for(auto it = m.held_keys.begin(); it != m.held_keys.end(); it++) {
+      size_t idx = distance(m.held_keys.begin(), it);
+      h ^= (std::hash<char>{}(*it) << idx);
+    }
+    return h;
+  }
+};
 
+vector<Maze> reconstruct_path(unordered_map<Maze, Maze> came_from, Maze last_node) {
+  vector<Maze> rv{last_node};
+  while (came_from.contains(last_node)) {
+    last_node = came_from[last_node];
+    rv.push_back(last_node);
+  }
+  return rv;
+}
+
+vector<Maze> astar(Maze start) {
+  unordered_map<Maze, Maze> came_from;
+  unordered_map<Maze, unsigned int> f_score;
+  unordered_map<Maze, unsigned int> g_score;
+  auto compare = [&f_score](auto a, auto b) { return f_score[a] > f_score[b]; }; // > so that lower f_scores are first in queue
+  priority_queue<Maze, vector<Maze>, decltype(compare)> open_set(compare);
+
+  g_score[start] = 0;
+  f_score[start] = start.f_score();
+  open_set.push(start);
+
+  while (open_set.size() > 0) {
+    auto cur_node = open_set.top();
+    /* cout << "DEBUG: cur_node.f_score = " << cur_node.f_score() << endl; */
+    open_set.pop();
+
+    if (cur_node.is_goal()) {
+      return reconstruct_path(came_from, cur_node);
+    }
+
+    for(auto next_node : cur_node.next_states()) {
+      unsigned int node_g_score = g_score[cur_node] + 1;
+
+      if (!g_score.contains(next_node) || node_g_score < g_score[next_node]) {
+        /* cout << "DEBUG: pushing next_node.f_score = " << next_node.f_score() << endl; */
+        came_from[next_node] = cur_node;
+        g_score[next_node] = node_g_score;
+        f_score[next_node] = node_g_score + next_node.f_score();
+        open_set.push(next_node); // can't see a good way to check the membership first, should I use a vector with make_heap, etc. directly?
+      }
+    }
+  }
+
+  return vector<Maze>{}; // failure
+}
 
 #ifndef IS_TEST
 int main(int argc, char** argv) {
-  cout << "hello world";
+  if (argc < 2) {
+    std::cerr << "Provide input filename as argument" << std::endl;
+    return 1;
+  }
+  ifstream fh{argv[1]};
+  auto maze = Maze::parse(fh);
+  auto p1_path = astar(maze);
+  cout << "p1: " << p1_path.size() - 1 << " steps to get all keys" << endl;
   return 0;
 }
 #endif
--- 2019/d18/spec.cpp
+++ 2019/d18/spec.cpp
@@ -23,3 +23,30 @@ TEST_CASE("parse", "[Maze]") {
 
   REQUIRE(maze.cur_pos == Point(15,1));
 }
+
+TEST_CASE("step_to", "[Maze]") {
+  auto in = istringstream{sample_input};
+  auto maze = Maze::parse(in);
+
+  // step_to doesn't validate that the dest is a neighbor of cur_pos, which I'm
+  // taking advantage of here. In production code step_to would verify that
+  // invariant here, but for AOC it's fine since I know `next_states` takes care
+  // of it, and it's convenient for a test here.
+  auto maze2 = maze.step_to(Point(17, 1));
+
+  REQUIRE(maze2.cur_pos == Point(17,1));
+  REQUIRE(maze.cur_pos == Point(15,1)); // original maze should not have changed
+
+  REQUIRE(maze2.held_keys.size() == 1);
+  REQUIRE(maze2.held_keys.contains('a'));
+  REQUIRE(maze.held_keys.size() == 0); // original maze should not have changed
+}
+
+TEST_CASE("search", "[astar]") {
+  auto in = istringstream{sample_input};
+  auto maze = Maze::parse(in);
+
+  auto path = astar(maze);
+
+  REQUIRE(path.size() == 87); // should be 86 steps, which means 87 states
+}
