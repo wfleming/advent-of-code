af0759ff8721d057d02db466c9de595cb5317f07
Will Fleming <will@flemi.ng>
2022-09-15 12:10:01 -0400

    cpp hand-implemented
    Given the search space I kind of didn't think even a hand-implemented
    brute force approach would work, but I figured I'd try it.
    
    It doesn't work. I mean, it's pretty fast, but there are 10s or 100s of
    billions of possible inputs (trillions?), and that's just not realistic
    to crunch through.
    
    There's got to be something to do with working backwards from the
    constrained ranges in the register values (e.g. often mod or div 26) to
    figure out what would validate.

--- 2021/d24/d24.cpp
+++ 2021/d24/d24.cpp
@@ -1,199 +1,198 @@
-#include <algorithm>
 #include <istream>
 #include <string>
-#include <variant>
-#include <vector>
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <functional>
-#include <optional>
 
 using namespace std;
 
-typedef variant<char, int> instr_arg_t;
-
-struct Instruction {
-  string instr;
-  char arg0;
-  instr_arg_t arg1;
-
-  Instruction(string& line) {
-    // no error handling, YOLO
-    auto instr_end_idx = line.find(" ");
-    instr = line.substr(0, instr_end_idx);
-    auto arg0_end_idx = line.find(" ", instr_end_idx + 1);
-    string arg0_str;
-    if (arg0_end_idx == string::npos) {
-      arg0 = line.substr(instr_end_idx + 1).front();
-    } else {
-      arg0 = line.substr(instr_end_idx + 1, arg0_end_idx - instr_end_idx + 1).front();
-    }
-
-    if (arg0_end_idx != string::npos) {
-      auto arg1_str = line.substr(arg0_end_idx + 1);
-      try {
-        arg1 = stoi(arg1_str);
-      } catch(invalid_argument const& ex) {
-        arg1 = arg1_str.front();
-      }
-    }
-  }
-
-  Instruction(string instr, char arg0, instr_arg_t arg1):
-    instr{instr}, arg0{arg0}, arg1{arg1} {}
-
-  bool operator ==(const Instruction& other) const = default;
-};
-
-vector<Instruction> parse_program(istream&& in) {
-  vector<Instruction> rv;
-
-  string line;
-  while (in.good()) {
-    getline(in, line);
-    rv.push_back(Instruction{line});
-  }
-
-  return rv;
-}
-
-struct AluErr {
-  string msg;
-
-  AluErr(const char* m): msg{m} {};
-};
-
-struct ALU {
-  int w = 0, x = 0, y = 0, z = 0; // registers
-  size_t ip = 0; // instruction pointer
-  vector<Instruction> program;
-  vector<int> inputs;
-
-  ALU(vector<Instruction> program): program{program} {}
-  ALU(vector<Instruction> program, vector<int> inputs): program{program}, inputs{inputs} {}
-
-  void set_reg(char name, int val) {
-    switch (name) {
-      case 'w': w = val; break;
-      case 'x': x = val; break;
-      case 'y': y = val; break;
-      case 'z': z = val; break;
-    }
-  }
-
-  int get_reg(char name) {
-    switch (name) {
-      case 'w': return w;
-      case 'x': return x;
-      case 'y': return y;
-      case 'z': return z;
-    }
-    return -1;
-  }
-
-  int get_lit_or_reg(instr_arg_t a) {
-    if (a.index() == 0) {
-      return get_reg(get<char>(a));
-    } else {
-      return get<int>(a);
-    }
-  }
-
-  void math_op(Instruction i, function<int(int, int)> op) {
-    int lhs = get_reg(i.arg0), rhs = get_lit_or_reg(i.arg1);
-    int r = op(lhs, rhs);
-    set_reg(i.arg0, r);
-  }
-
-  void step() {
-    auto instr = program[ip];
-
-    if (instr.instr == "inp") {
-      if (inputs.empty()) {
-        throw AluErr{"requested input but there is no input"};
-      } else {
-        set_reg(instr.arg0, inputs.front());
-        inputs.erase(inputs.begin());
-      }
-    } else if (instr.instr == "add") {
-      math_op(instr, [](auto a, auto b) { return a + b; } );
-    } else if (instr.instr == "mul") {
-      math_op(instr, [](auto a, auto b) { return a * b; } );
-    } else if (instr.instr == "div") {
-      math_op(instr, [](auto a, auto b) {
-        if ( b == 0) { throw AluErr{"Illegal divide by zero"}; }
-        return a / b;
-      } );
-    } else if (instr.instr == "mod") {
-      math_op(instr, [](auto a, auto b) {
-        if ( a < 0 || b <= 0) { throw AluErr{"Illegal mod operation"}; }
-        return a % b;
-      } );
-    } else if (instr.instr == "eql") {
-      math_op(instr, [](auto a, auto b) { return a == b; } );
-    } else {
-      throw AluErr{"unknown instruction"};
-    }
-
-    ip++;
-  }
-
-  optional<AluErr> run_until_halt_or_error() {
-    while (ip < program.size()) {
-      try {
-        step();
-      } catch (const AluErr& e) {
-        return e;
-      }
-    }
-
-    return optional<AluErr>{};
-  }
-};
-
-vector<int> split_input(long x) {
-  vector<int> xs{};
-  while (x > 0) {
-    xs.insert(xs.begin(), x % 10);
-    x = x / 10;
-  }
-  return xs;
-}
-
-bool input_is_valid(vector<int> input) {
-  return input.size() == 14 && find(input.begin(), input.end(), 0) == input.end();
-}
-
-bool validate_model_number(const vector<Instruction>& prog, int n) {
-  auto inputs = split_input(n);
-  if (!input_is_valid(inputs)) { return false; }
-  auto alu = ALU{prog, inputs};
-  auto r = alu.run_until_halt_or_error();
-  if (r.has_value()) {
-    cerr << "The model number " << n << " crashed the computer: " << (*r).msg << endl;
+bool input_is_valid(unsigned long n) {
+  if (n > 99999999999999) {
     return false;
   }
-  return alu.z == 0;
+
+  if (n < 11111111111111) {
+    return false;
+  }
+
+  while (n > 0) {
+    if ((n % 10) == 0) { return false; }
+    n = n / 10;
+  }
+
+  return true;
 }
 
-long p1(const vector<Instruction>& prog) {
-  for(long x = 99999999999999; x >= 11111111111111; x--) {
-    if (validate_model_number(prog, x)) {
+bool run_monad(unsigned long n) {
+  // w,x,y,z are the registers.
+  int w = 0, x = 0, y = 0, z = 0;
+
+  // logic for each digit is the same, just different constants (moduli,
+  // dividing). digit 1 can omit a lot of logic since everything is 0 to start.
+  //
+  // digit 1: z = w + 13 (z in 14..22)
+  // digit 2: check if w != z + 10, y = 25 or 26, z = z * y + (w + 16)
+
+  // digit 1
+  w = n % 10;
+  z = w + 13;
+
+  // digit 2
+  n = n / 10;
+  w = n % 10;
+  x = z + 10; //(z % 26) + 10;, but z must be < 26 here, so mod is pointless
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 16) * x;
+  z += y;
+
+  // digit 3
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) + 10;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 2) * x;
+  z += y;
+
+  // digit 4
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) + 10;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 8) * x;
+  z += y;
+
+  // digit 5
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) + 14;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 11) * x;
+  z += y;
+
+  // digit 6
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) - 11;
+  z = z / 26; // first time this isn't 1
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 11) * x;
+  z += y;
+
+  // digit 7
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) + 10;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 12) * x;
+  z += y;
+
+  // digit 8
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) - 16;
+  z = z / 26;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 2) * x;
+  z += y;
+
+  // digit 9
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) - 9;
+  z = z / 26;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 2) * x;
+  z += y;
+
+  // digit 10
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) + 11;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 15) * x;
+  z += y;
+
+  // digit 11
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) - 8;
+  z = z / 26;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 1) * x;
+  z += y;
+
+  // digit 12
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) - 8;
+  z = z / 26;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 10) * x;
+  z += y;
+
+  // digit 13
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) - 10;
+  z = z / 26;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 14) * x;
+  z += y;
+
+  // digit 14
+  n = n / 10;
+  w = n % 10;
+  x = (z % 26) - 9;
+  z = z / 26;
+  x = (int)(x != w);
+  y = (25 * x) + 1;
+  z = z * y;
+  y = (w + 10) * x;
+  z += y;
+
+  return z == 0;
+}
+
+unsigned long p1() {
+  unsigned long it = 0;
+
+  for(unsigned long x = 99999999999999; x >= 11111111111111; x--) {
+    it++;
+    if (it % 1000000 == 0) { cout << '.'; } // track progress every 1 million numbers
+    if (input_is_valid(x) && run_monad(x)) {
       return x;
     }
   }
 
-  return -1;
+  return 0;
 }
 
 #ifndef IS_TEST
-int main(int argc, char** argv) {
-  if (argc < 2) {
-    cerr << "Error: no input filename provided." << endl;
-    return 1;
-  }
-  auto prog = parse_program(ifstream{argv[1]});
-  cout << "p1: " << p1(prog) << " is the largest valid model number" << endl;
+int main(int, char** ) {
+  cout << "p1: " << p1() << " is the largest valid model number" << endl;
 }
 #endif
--- 2021/d24/test.cpp
+++ 2021/d24/test.cpp
@@ -1,106 +1,20 @@
 #include <cassert>
 #include "d24.cpp"
 
-void test_parse_instr() {
-  auto s = string{"inp a"};
-  auto i = Instruction{s};
-  assert(i.instr == "inp");
-  assert(i.arg0 == 'a');
+void test_input_is_valid() {
+  assert(input_is_valid(12345678912345));
 
-  s = string{"mul x b"};
-  i = Instruction{s};
-  assert(i.instr == "mul");
-  char* arg1_c = get_if<char>(&i.arg1);
-  assert(i.arg0 == 'x');
-  assert(arg1_c != NULL && *arg1_c == 'b');
+  assert(!input_is_valid(12340678912345));
 
-  s = string{"zed w -300"};
-  i = Instruction{s};
-  assert(i.instr == "zed");
-  int* arg1_i = get_if<int>(&i.arg1);
-  assert(i.arg0 == 'w');
-  assert(arg1_i != NULL && *arg1_i == -300);
+  assert(!input_is_valid(3));
 }
 
-void test_parse_program() {
-  const char* sample_input = R"(inp z
-inp x
-mul z 3
-eql z x)";
-  auto prog = parse_program(istringstream{sample_input});
-
-  /* vector<Instruction> expected; */
-  assert((prog == vector<Instruction>{
-      Instruction{(string{"inp"}), 'z', instr_arg_t{}},
-      Instruction{(string{"inp"}), 'x', instr_arg_t{}},
-      Instruction{(string{"mul"}), 'z', 3},
-      Instruction{(string{"eql"}), 'z', 'x'},
-  }));
-}
-
-void test_input() {
-  vector<int> xs = split_input(12345678912345);
-  assert((xs == vector<int>{1,2,3,4,5,6,7,8,9,1,2,3,4,5}));
-  assert(input_is_valid(xs));
-
-  xs = split_input(12340678912345);
-  assert((xs == vector<int>{1,2,3,4,0,6,7,8,9,1,2,3,4,5}));
-  assert(!input_is_valid(xs));
-
-  xs = split_input(3);
-  assert(!input_is_valid(xs));
-}
-
-void test_run_program() {
-  // the example program that converts input to binary
-  const char* sample_input = R"(inp w
-add z w
-mod z 2
-div w 2
-add y w
-mod y 2
-div w 2
-add x w
-mod x 2
-div w 2
-mod w 2)";
-  auto prog = parse_program(istringstream{sample_input});
-  auto alu = ALU(prog, vector<int>{10});
-  assert(!alu.run_until_halt_or_error().has_value());
-  assert(alu.z == 0); // 1s place
-  assert(alu.y == 1); // 2s place
-  assert(alu.x == 0); // 4s place
-  assert(alu.w == 1); // 8s place
-}
-
-void test_program_multiple_input() {
-  const char* sample_input = R"(inp w
-inp x
-mul w x)";
-  auto prog = parse_program(istringstream{sample_input});
-  auto alu = ALU(prog, vector<int>{3,4});
-  assert(!alu.run_until_halt_or_error().has_value());
-  assert(alu.w == 12);
-}
-
-void test_program_eql() {
-  const char* sample_input = R"(inp w
-inp x
-mul w x
-eql w 12)";
-  auto prog = parse_program(istringstream{sample_input});
-  auto alu = ALU(prog, vector<int>{3,4});
-  assert(!alu.run_until_halt_or_error().has_value());
-  assert(alu.w == 1);
+void test_run_monad() {
 }
 
 int main(int, char**) {
-  test_parse_instr();
-  test_parse_program();
-  test_input();
-  test_run_program();
-  test_program_multiple_input();
-  test_program_eql();
+  test_input_is_valid();
+  test_run_monad();
 
   cout << "Tests complete." << endl;
 }
