c42d7fd1a3d6f0e92412a9b6a379f0e9179fc078
Will Fleming <will@flemi.ng>
2022-09-19 18:26:37 -0400

    2019 d19 binary search
    Still not "fast", but certainly "faster". 19s for both parts. That's
    enough optimizing for me for now.

--- 2019/d19/src/beam.rs
+++ 2019/d19/src/beam.rs
@@ -29,7 +29,7 @@ impl TractorBeam {
         self.points[pt]
     }
 
-    pub fn is_pt_ship(&mut self, pt: &(u32,u32)) -> bool {
+    fn is_pt_ship(&mut self, pt: &(u32,u32)) -> bool {
         for xd in 0..SHIP_SIZE {
             for yd in 0..SHIP_SIZE {
               if !self.check_pt(&(pt.0 + xd, pt.1 + yd)) {
@@ -41,22 +41,42 @@ impl TractorBeam {
         true
     }
 
-    // returns top-left pt of ship if found
-    pub fn find_ship(&mut self) -> Option<(u32, u32)> {
-        for y in SHIP_SIZE..(SHIP_SIZE * 20) {
-            // these constants based on my p1 output & ratios emitted, plus some leeway
-            let min_x: u32 = (y as f32 * 0.7).floor() as u32;
-            let max_x: u32 = (y as f32 * 0.9).floor() as u32;
-            if max_x - min_x < SHIP_SIZE {
-                continue;
-            }
-            for x in min_x..max_x {
-                if self.is_pt_ship(&(x,y)) {
-                    return Some((x,y));
-                }
+    fn y_fits_ship(&mut self, y: u32) -> Option<(u32, u32)> {
+        // these constants based on my p1 output & ratios emitted, plus some leeway
+        let min_x: u32 = (y as f32 * 0.7).floor() as u32;
+        let max_x: u32 = (y as f32 * 0.9).floor() as u32;
+        if max_x - min_x < SHIP_SIZE {
+            return None;
+        }
+        for x in min_x..max_x {
+            if self.is_pt_ship(&(x,y)) {
+                return Some((x,y));
             }
         }
 
         None
+    }
+
+    fn bin_search_ship(&mut self, min_y: u32, max_y: u32) -> Option<(u32, u32)> {
+        if max_y - min_y < 2 {
+            let mut r = self.y_fits_ship(min_y);
+            if r.is_none() {
+                r = self.y_fits_ship(max_y);
+            }
+
+            return r;
+        }
+
+        let mid = min_y + ((max_y - min_y) / 2);
+        if self.y_fits_ship(mid).is_some() {
+            return self.bin_search_ship(min_y, mid);
+        } else {
+            return self.bin_search_ship(mid, max_y);
+        }
+    }
+
+    // returns top-left pt of ship if found
+    pub fn find_ship(&mut self) -> Option<(u32, u32)> {
+        self.bin_search_ship(SHIP_SIZE, SHIP_SIZE * 15)
     }
 }
