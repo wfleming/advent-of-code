950cb740bf80840e1b0bd09088ce43c934334484
Will Fleming <will@flemi.ng>
2022-09-19 17:32:10 -0400

    2019 d19 p2
    The brute-force approach isn't fast (about a minute for both parts), but
    it was easy and fast enough I got an answer, so I'm moving on.
    
    I know my intcode impl has a fair amount of overhead due to BigInt
    support, though I don't know how much impact that's having here. I guess
    I consider that a vote for just copying shared code day-to-day and not
    actually extracting to a shared module like I chose to do in 2019.
    
    A binary search likely would speed things up a lot rather than just
    starting at a given y and iterating up. Maybe I'll try that later to see
    how much it helps.

--- 2019/d19/src/beam.rs
+++ 2019/d19/src/beam.rs
@@ -1,34 +1,62 @@
-use std::collections::HashSet;
+use std::collections::BTreeMap;
 use intcode::{machine::Machine, tape::Tape};
 use intcode::num_traits::ToPrimitive;
 use intcode::num_bigint::BigInt;
 use intcode::num_traits::cast::FromPrimitive;
-// use std::fmt;
 
-#[derive(Debug)]
 pub struct TractorBeam {
     tape: Tape,
-    pub points: HashSet<(i64,i64)>,
+    pub points: BTreeMap<(u32,u32), bool>,
 }
+
+const SHIP_SIZE: u32 = 100;
 
 impl TractorBeam {
     pub fn new(tape: Tape) -> TractorBeam {
-        TractorBeam{tape: tape, points: HashSet::new()}
+        TractorBeam{tape: tape, points: BTreeMap::new()}
     }
 
-    pub fn scan_area(&mut self, max_x: i64, max_y: i64) {
-        for x in 0..(max_x + 1) {
-            for y in 0..(max_y + 1) {
-                // println!("scanning {}, {}", x, y);
-                let mut m = Machine::new(self.tape.clone());
-                m.push_input(BigInt::from_i64(x).unwrap());
-                m.push_input(BigInt::from_i64(y).unwrap());
-                m.run_until_exit();
+    pub fn check_pt(&mut self, pt: &(u32, u32)) -> bool {
+        if !self.points.contains_key(pt) {
+            let mut m = Machine::new(self.tape.clone());
+            m.push_input(BigInt::from_u32(pt.0).unwrap());
+            m.push_input(BigInt::from_u32(pt.1).unwrap());
+            m.run_until_exit();
 
-                if m.outputs[0].to_i32() == Some(1) {
-                    self.points.insert((x,y));
+            self.points.insert(pt.clone(), m.outputs[0].to_u32() == Some(1));
+        }
+
+        self.points[pt]
+    }
+
+    pub fn is_pt_ship(&mut self, pt: &(u32,u32)) -> bool {
+        for xd in 0..SHIP_SIZE {
+            for yd in 0..SHIP_SIZE {
+              if !self.check_pt(&(pt.0 + xd, pt.1 + yd)) {
+                  return false;
+              }
+            }
+        }
+
+        true
+    }
+
+    // returns top-left pt of ship if found
+    pub fn find_ship(&mut self) -> Option<(u32, u32)> {
+        for y in SHIP_SIZE..(SHIP_SIZE * 20) {
+            // these constants based on my p1 output & ratios emitted, plus some leeway
+            let min_x: u32 = (y as f32 * 0.7).floor() as u32;
+            let max_x: u32 = (y as f32 * 0.9).floor() as u32;
+            if max_x - min_x < SHIP_SIZE {
+                continue;
+            }
+            for x in min_x..max_x {
+                if self.is_pt_ship(&(x,y)) {
+                    return Some((x,y));
                 }
             }
         }
+
+        None
     }
 }
--- 2019/d19/src/main.rs
+++ 2019/d19/src/main.rs
@@ -1,9 +1,47 @@
 mod beam;
 
-// use intcode::num_bigint::BigInt;
-// use intcode::num_traits::cast::FromPrimitive;
 use intcode::tape;
 use std::env::args;
+
+fn print_p1_pts(beam: &mut beam::TractorBeam)  {
+    let mut min_ratio = 200.0;
+    let mut max_ratio = 0.0;
+
+    for y in 0..50 {
+        for x in 0..50 {
+            if beam.check_pt(&(x,y)) {
+                print!("o");
+
+                let r = x as f32 / y as f32;
+                if r < min_ratio {
+                    min_ratio = r;
+                }
+                if r > max_ratio {
+                    max_ratio = r;
+                }
+            } else {
+                print!(".");
+            }
+        }
+        print!("\n")
+    }
+
+
+    println!("The min x ratio was {}, the max was {}", min_ratio, max_ratio);
+}
+
+fn p1(beam: &mut beam::TractorBeam) -> u32 {
+    let mut rv = 0;
+    for y in 0..50 {
+        for x in 0..50 {
+            if beam.check_pt(&(x,y)) {
+                 rv += 1;
+            }
+        }
+    }
+
+    rv
+}
 
 fn main() {
     let mut argv = args();
@@ -11,9 +49,16 @@ fn main() {
     let input = argv.next().expect("provide a filename");
     let tape = tape::from_file(&input);
 
-    // p1
     let mut b = beam::TractorBeam::new(tape.clone());
-    b.scan_area(49, 49);
-    println!("p1: there are {} points affected ", b.points.len());
-
+    println!("p1: there are {} points affected", p1(&mut b));
+    print_p1_pts(&mut b);
+    let s = b.find_ship();
+    match s {
+        Some(p) => {
+            println!("p2: ship starts at ({},{})", p.0, p.1);
+            let a = (p.0 * 10000) + p.1;
+            println!("p2: answer is {}", a);
+        },
+        None => println!("p2: didn't find the ship"),
+    }
 }
