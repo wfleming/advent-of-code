a4dd40f280ee762f542041445395b9860ceabc28
Will Fleming <will@flemi.ng>
2022-09-22 18:38:27 -0400

    2019 d23
    p1 was pretty straightforward.
    
    p2 took a bit of massaging to get the definition of "idle" correct.
    
    My first take was just to check at the end of the loop if all machines
    were waiting for input or just had the filler in their queue. That was
    wrong because when a machine was waiting for input I pushed -1 as input
    and then immediately stepped it. So of course it was no longer waiting
    for input and no longer had -1 in its input queue.
    
    Counting a machine as becoming idle when it gets -1 and becoming active
    when it outputs a packet or receives a packet worked well. I think it
    didn't work when I only did it for output - receiving a packet was also
    important.
    
    Also: my first time reaching for RefCell in rust. I think this is the
    right way to mutate items in a vector while you're iterating over
    it?

---
+++ 2019/d23/src/main.rs
@@ -0,0 +1,125 @@
+use intcode::num_bigint::BigInt;
+use intcode::num_traits::cast::{FromPrimitive, ToPrimitive};
+use intcode::num_traits::Zero;
+use intcode::{machine::Machine, tape};
+use std::cell::RefCell;
+use std::env::args;
+
+fn nat_address() -> BigInt { BigInt::from_i32(255).unwrap() }
+fn filler_input() -> BigInt { BigInt::from_i32(-1).unwrap() }
+
+fn construct_computers(instrs: &tape::Tape) -> Vec<RefCell<Machine>> {
+    let mut computers = Vec::with_capacity(50);
+    for i in 0..50 {
+        let mut m = Machine::new(instrs.clone());
+        m.push_input(BigInt::from_usize(i).unwrap());
+        computers.push(RefCell::new(m));
+    }
+
+    computers
+}
+
+fn simulate_computers_p1(instrs: &tape::Tape) -> i32 {
+    let computers = construct_computers(instrs);
+
+    loop {
+        for cell in &computers {
+            if cell.borrow().is_exited() {
+                panic!("I didn't expect a machine to exit");
+            }
+
+            if cell.borrow().is_waiting_for_input() {
+                cell.borrow_mut().push_input(filler_input());
+            }
+
+            let oc = cell.borrow().outputs.len();
+            cell.borrow_mut().step();
+            if cell.borrow().outputs.len() > oc {
+                // a packet started, simulate this machine until it finishes the packet
+                while cell.borrow().outputs.len() < oc + 3 {
+                    if cell.borrow().is_waiting_for_input() {
+                        panic!("I didn't expect a machine to ask for input in the middle of emitting a packet");
+                    }
+                    cell.borrow_mut().step();
+                }
+
+                let oc = cell.borrow().outputs.len();
+                let (a, x, y) = (cell.borrow().outputs[oc - 3].clone(), cell.borrow().outputs[oc - 2].clone(), cell.borrow().outputs[oc - 1].clone());
+                if a == nat_address() { // p1 solution
+                    return y.to_i32().unwrap();
+                } else {
+                    computers[a.to_usize().unwrap()].borrow_mut().push_input(x);
+                    computers[a.to_usize().unwrap()].borrow_mut().push_input(y);
+                }
+            }
+        }
+    }
+}
+
+fn simulate_computers_p2(instrs: &tape::Tape) -> i32 {
+    let computers = construct_computers(instrs);
+    let mut idle = vec![false; 50];
+    let mut nat_packet: (BigInt, BigInt) = (Zero::zero(), Zero::zero());
+    let mut last_nat_y: BigInt = Zero::zero();
+
+    loop {
+        for (i, cell) in computers.iter().enumerate() {
+            if cell.borrow().is_exited() {
+                panic!("I didn't expect a machine to exit");
+            }
+
+            let oc = cell.borrow().outputs.len();
+            if cell.borrow().is_waiting_for_input() {
+                idle[i] = true;
+                cell.borrow_mut().push_input(filler_input());
+            } else {
+                cell.borrow_mut().step();
+            }
+            if cell.borrow().outputs.len() > oc {
+                idle[i] = false;
+                // a packet started, simulate this machine until it finishes the packet
+                while cell.borrow().outputs.len() < oc + 3 {
+                    if cell.borrow().is_waiting_for_input() {
+                        panic!("I didn't expect a machine to ask for input in the middle of emitting a packet");
+                    }
+                    cell.borrow_mut().step();
+                }
+
+                let oc = cell.borrow().outputs.len();
+                let (a, x, y) = (cell.borrow().outputs[oc - 3].clone(), cell.borrow().outputs[oc - 2].clone(), cell.borrow().outputs[oc - 1].clone());
+                if a == nat_address() {
+                    nat_packet = (x,y)
+                } else {
+                    computers[a.to_usize().unwrap()].borrow_mut().push_input(x);
+                    computers[a.to_usize().unwrap()].borrow_mut().push_input(y);
+                    idle[a.to_usize().unwrap()] = false;
+                }
+            }
+        }
+
+        let all_idle = idle.iter().all(|x| *x);
+        if all_idle && nat_packet.1 != Zero::zero() {
+            // send the nat packet to address 0
+            if nat_packet.1 == last_nat_y { // p2 answer
+                return last_nat_y.to_i32().unwrap();
+            }
+
+            last_nat_y = nat_packet.1.clone();
+            computers[0].borrow_mut().push_input(nat_packet.0.clone());
+            computers[0].borrow_mut().push_input(nat_packet.1.clone());
+            idle[0] = false;
+        }
+    }
+}
+
+fn main() {
+    let mut argv = args();
+    argv.next();
+    let input = argv.next().expect("provide a filename");
+    let instrs = tape::from_file(&input);
+
+    let p1 = simulate_computers_p1(&instrs);
+    println!("p1: {}", p1);
+    let p2 = simulate_computers_p2(&instrs);
+    println!("p2: {}", p2);
+}
