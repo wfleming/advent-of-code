3d734d3f81a0b67a008cbea1b7dde1c0cb2126d2
Will Fleming <will@flemi.ng>
2022-12-03 14:55:24 -0500

    2022 d04: convert panics to results
    Just playing since I don't ever really get to write "real" Rust code
    where I need to worry about errors and not just crash at the first sign
    of trouble.
    
    Still have 1 expect in `main`, which seems reasonable since it's pretty
    equivalent to doing a `match` and logging the error there.
    
    And 1 `unwrap` when parsing. I'm not sure what the appropriate pattern
    here is, and  there may not be an elegant idiomatic solution. It looks
    like I could `collect` a vec of results into a result of a vec[1] (so
    like `let lines: Vec<&str, _> = br.lines().collect()?`), but the whole
    reason I've been using `BufReader` is to avoid effectively mapping the
    whole file into memory at once (not that it really matters at this
    scale, I'm just being picky), so at that point I might as well just use
    `fs::read_to_string`. I think if I wanted to maintain the more optimal
    memory usage of `BufReader` and convert this to return a result rather
    than panic I'd probably just need to do a for loop that appends each
    element to a `mut Vec<Rucksack>` as it goes, and then return the result.
    It'd be nice if `?` inside a closure could "hoist" to the outer
    function, but I can see why they don't do that - that would be skirting
    pretty close to behaving like exceptions.
    
    I don't think I'll stick with this for later days. It's a useful
    exercise, but it does lead to slightly more complex code and for AOC the
    benefit isn't worth it.
    
    [1]: https://stackoverflow.com/questions/63798662/how-do-i-convert-a-vecresultt-e-to-resultvect-e

--- 2022/d03/src/main.rs
+++ 2022/d03/src/main.rs
@@ -2,13 +2,13 @@ use std::env::args;
 use std::fs;
 use std::io::{self, BufRead};
 
-fn priority(c: char) -> u32 {
+fn priority(c: char) -> Result<u32, String> {
     if c.is_ascii_lowercase() {
-        (c as u32) - 96 // 'a' is 97
+        Ok((c as u32) - 96) // 'a' is 97
     } else if c.is_ascii_uppercase() {
-        (c as u32) - 38 // 'A' is 65
+        Ok((c as u32) - 38) // 'A' is 65
     } else {
-        panic!("Invalid character, no defined priority")
+        Err("Invalid character, no defined priority".to_string())
     }
 }
 
@@ -20,35 +20,35 @@ impl Rucksack {
         Rucksack(chars)
     }
 
-    pub fn shared_item(&self) -> char {
+    pub fn shared_item(&self) -> Result<char, String> {
         let c1 = &self.0[0..(self.0.len() / 2)];
         let c2 = &self.0[(self.0.len() / 2)..];
 
         for i1 in c1 {
             for i2 in c2 {
                 if i1 == i2 {
-                    return *i1;
+                    return Ok(*i1);
                 }
             }
         }
-        panic!("no common item found")
+        Err("no common item found".to_string())
     }
 }
 
-fn parse_input(input_path: &str) -> Vec<Rucksack> {
-    let f = fs::File::open(input_path).expect("Input path should exist");
+fn parse_input(input_path: &str) -> Result<Vec<Rucksack>, String> {
+    let f = fs::File::open(input_path).map_err(|e| format!("{}", e))?;
     let br = io::BufReader::new(f);
 
-    br.lines().map(|l| Rucksack::parse(&l.unwrap())).collect()
+    Ok(br.lines().map(|l| Rucksack::parse(&l.unwrap())).collect())
 }
 
-fn p1(rucksacks: &Vec<Rucksack>) -> u32 {
-    rucksacks.iter().map(|r| priority(r.shared_item())).sum()
+fn p1(rucksacks: &Vec<Rucksack>) -> Result<u32, String> {
+    rucksacks.iter().map(|r| priority(r.shared_item()?)).sum()
 }
 
-fn group_badge(rucksacks: &[Rucksack]) -> char {
+fn group_badge(rucksacks: &[Rucksack]) -> Result<char, String> {
     if rucksacks.len() != 3 {
-        panic!("Unexpected slice size");
+        return Err("Unexpected slice size".to_string());
     }
 
     for i1 in &rucksacks[0].0 {
@@ -58,19 +58,19 @@ fn group_badge(rucksacks: &[Rucksack]) -> char {
             }
             for i3 in &rucksacks[2].0 {
                 if i1 == i2 && i1 == i3 {
-                    return *i1;
+                    return Ok(*i1);
                 }
             }
         }
     }
-    panic!("No badge found in group");
+    Err("No badge found in group".to_string())
 }
 
-fn p2(rucksacks: &Vec<Rucksack>) -> u32 {
+fn p2(rucksacks: &Vec<Rucksack>) -> Result<u32, String> {
     rucksacks
         .as_slice()
         .chunks(3)
-        .map(|g| priority(group_badge(g)))
+        .map(|g| priority(group_badge(g)?))
         .sum()
 }
 
@@ -78,10 +78,20 @@ fn main() {
     let mut argv = args();
     argv.next();
     let input_path = argv.next().expect("provide a filename");
-    let rucksacks = parse_input(&input_path);
+    let rucksacks = match parse_input(&input_path) {
+        Ok(rs) => rs,
+        Err(e) => panic!("Error parsing input: {}", e),
+    };
 
-    println!("p1: summed priorities of shared items: {}", p1(&rucksacks));
-    println!("p1: summed priorities of badges: {}", p2(&rucksacks));
+    match p1(&rucksacks) {
+        Ok(x) => println!("p1: summed priorities of shared items: {}", x),
+        Err(e) => panic!("p1: error: {}", e),
+    }
+
+    match p2(&rucksacks) {
+        Ok(x) => println!("p1: summed priorities of badges: {}", x),
+        Err(e) => panic!("p2: error: {}", e),
+    }
 }
 
 #[cfg(test)]
@@ -90,15 +100,15 @@ mod tests {
 
     #[test]
     fn test_priority() {
-        assert_eq!(priority('a'), 1);
-        assert_eq!(priority('z'), 26);
-        assert_eq!(priority('A'), 27);
-        assert_eq!(priority('Z'), 52);
+        assert_eq!(priority('a'), Ok(1));
+        assert_eq!(priority('z'), Ok(26));
+        assert_eq!(priority('A'), Ok(27));
+        assert_eq!(priority('Z'), Ok(52));
     }
 
     #[test]
     fn test_shared_item() {
         let r = Rucksack::parse("vJrwpWtwJgWrhcsFMMfFFhFp");
-        assert_eq!(r.shared_item(), 'p');
+        assert_eq!(r.shared_item(), Ok('p'));
     }
 }
