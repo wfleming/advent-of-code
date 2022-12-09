5241d4a4cc1e2c137d7c27bf11063a4b9353bc7f
Will Fleming <will@flemi.ng>
2022-12-09 11:50:45 -0500

    2022 d09 p2

--- 2022/d09/src/main.rs
+++ 2022/d09/src/main.rs
@@ -1,7 +1,7 @@
+use std::collections::BTreeSet;
 use std::env::args;
 use std::fs;
 use std::ops::{Add, Sub};
-use std::collections::BTreeSet;
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Point {
@@ -33,7 +33,7 @@ impl Sub for Point {
 
 struct Move {
     dir: Point,
-    count: u8
+    count: u8,
 }
 
 impl Move {
@@ -48,7 +48,10 @@ impl Move {
             _ => panic!("unexpected char in move"),
         };
 
-        Move { dir: dir, count: count }
+        Move {
+            dir: dir,
+            count: count,
+        }
     }
 
     pub fn parse_all(input: &str) -> Vec<Move> {
@@ -56,48 +59,42 @@ impl Move {
     }
 }
 
-struct Rope {
-    h: Point,
-    t: Point
-}
+struct Rope(Vec<Point>); // head is first knot in vec
 
 impl Rope {
-    pub fn new() -> Self {
-        Rope { h: Point { x: 0, y: 0 }, t: Point { x: 0, y: 0 } }
-    }
-
-    pub fn apply_move(&mut self, m: &Move) {
-        for _ in 0..m.count {
-            self.step(&m.dir);
-        }
+    pub fn new(knots: usize) -> Self {
+        let knots = (0..knots).map(|_| Point { x: 0, y: 0 }).collect::<Vec<_>>();
+        Rope(knots)
     }
 
     pub fn step(&mut self, dir: &Point) {
-        self.h = self.h + *dir;
+        self.0[0] = self.0[0] + *dir;
 
-        let mut diff = self.h - self.t;
-        if diff.x.abs() > 1 || diff.y.abs() > 1 {
-            if diff.x != 0 {
-                diff.x = diff.x / diff.x.abs();
+        for idx in 0..self.0.len() - 1 {
+            let mut diff = self.0[idx] - self.0[idx + 1];
+            if diff.x.abs() > 1 || diff.y.abs() > 1 {
+                if diff.x != 0 {
+                    diff.x = diff.x / diff.x.abs();
+                }
+                if diff.y != 0 {
+                    diff.y = diff.y / diff.y.abs();
+                }
+                self.0[idx + 1] = self.0[idx + 1] + diff;
             }
-            if diff.y != 0 {
-                diff.y = diff.y / diff.y.abs();
-            }
-            self.t = self.t + diff;
         }
     }
 }
 
-fn p1(moves: &Vec<Move>) -> usize {
-    let mut rope = Rope::new();
+fn count_tail_points(knots: usize, moves: &Vec<Move>) -> usize {
+    let mut rope = Rope::new(knots);
     let mut tail_pts: BTreeSet<Point> = BTreeSet::new();
 
-    tail_pts.insert(rope.t);
+    tail_pts.insert(rope.0[knots - 1]);
 
     for m in moves {
         for _ in 0..m.count {
             rope.step(&m.dir);
-            tail_pts.insert(rope.t);
+            tail_pts.insert(rope.0[knots - 1]);
         }
     }
 
@@ -114,7 +111,8 @@ fn main() {
         .to_string();
     let moves = Move::parse_all(&input);
 
-    println!("p1: tail touches {} points", p1(&moves));
+    println!("p1: tail touches {} points", count_tail_points(2, &moves));
+    println!("p2: tail touches {} points", count_tail_points(10, &moves));
 }
 
 #[cfg(test)]
@@ -124,19 +122,36 @@ mod tests {
     #[test]
     fn test_p1_one_move() {
         let ms = Move::parse_all("R 4");
-        assert_eq!(p1(&ms), 4);
+        assert_eq!(count_tail_points(2, &ms), 4);
     }
 
     #[test]
     fn test_p1_sample() {
-        let ms = Move::parse_all("R 4\n\
+        let ms = Move::parse_all(
+            "R 4\n\
             U 4\n\
             L 3\n\
             D 1\n\
             R 4\n\
             D 1\n\
             L 5\n\
-            R 2");
-        assert_eq!(p1(&ms), 13);
+            R 2",
+        );
+        assert_eq!(count_tail_points(2, &ms), 13);
+    }
+
+    #[test]
+    fn test_p2_sample() {
+        let ms = Move::parse_all(
+            "R 5\n\
+            U 8\n\
+            L 8\n\
+            D 3\n\
+            R 17\n\
+            D 10\n\
+            L 25\n\
+            U 20",
+        );
+        assert_eq!(count_tail_points(10, &ms), 36);
     }
 }
