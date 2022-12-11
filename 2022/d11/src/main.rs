f1cf9846fa1695072115490e34203dc02999329f
Will Fleming <will@flemi.ng>
2022-12-11 09:27:15 -0500

    2022 d11 p2
    The "keep your worry levels manageable" bit was tricky. I realized we
    needed to take the modulo of the item worry level to something, but
    wasn't sure how to figure out the "something" so that the remainder
    would still be congruent for all the other monkeys. I.e. if monkey 0
    tests mod 3 and another monkey tests mod 7, you need to mod by something
    such that future mod operations on 3 & 7 would have the same result.
    
    All the numbers the monkeys used for their modulo tests are prime, so
    there's probably some theorem I'm not thinking of that is applicable
    here. But I was pretty sure if I just used the lcm of all the monkey
    modulo test numbers that would also work. Rust stdlib doesn't have
    an `lcm` that I know of & I didn't want to write one, so I just
    multiplied all the numbers together, which is at least *a* CM, if not
    *the* LCM.
    
    Oh, wait: because the numbers are all prime just multiplying them
    together *is* the LCM. Every number has a unique prime factorization, so
    therefore the LCM of a bunch of primes is just their product.
    
    So yes, calculating the LCM and then modding the numbers by that works.
    It did require me to use u128s (u64s still overflowed), so I bet there
    is another more clever solution that keeps things even smaller.
    
    Reminding myself of the Chinese Remainder Theorem, I think that's
    applicable here & could be used to determine the modulus of a number
    with all the prime divisors here without actually carrying around the
    large LCM & dealing with larger numbers. But thankfully rust does have
    native 128 bit types and the somewhat naive LCM approach worked, so I'm
    calling it good.

--- 2022/d11/src/main.rs
+++ 2022/d11/src/main.rs
@@ -6,18 +6,18 @@ use std::fs;
 #[derive(Clone)]
 enum OpArg {
     Old,
-    Lit(u32),
+    Lit(u128),
 }
 
 impl OpArg {
     pub fn parse(input: &str) -> Self {
         match input {
             "old" => Self::Old,
-            _ => Self::Lit(input.parse::<u32>().unwrap()),
+            _ => Self::Lit(input.parse::<u128>().unwrap()),
         }
     }
 
-    pub fn val(&self, old: u32) -> u32 {
+    pub fn val(&self, old: u128) -> u128 {
         match self {
             Self::Old => old,
             Self::Lit(x) => *x,
@@ -33,7 +33,7 @@ enum Op {
 
 impl Op {
     pub fn parse(input: &str) -> Self {
-        let op_pieces: Vec<&str> = input.split(" ").collect();
+        let op_pieces: Vec<&str> = input.split(' ').collect();
         match op_pieces[0] {
             "+" => Self::Add(OpArg::parse(op_pieces[1])),
             "*" => Self::Mul(OpArg::parse(op_pieces[1])),
@@ -41,7 +41,7 @@ impl Op {
         }
     }
 
-    pub fn apply(&self, x: u32) -> u32 {
+    pub fn apply(&self, x: u128) -> u128 {
         match self {
             Self::Add(arg) => x + arg.val(x),
             Self::Mul(arg) => x * arg.val(x),
@@ -51,12 +51,12 @@ impl Op {
 
 #[derive(Clone)]
 struct Monkey {
-    items: VecDeque<u32>,
+    items: VecDeque<u128>,
     inspect_op: Op,
-    test_mod: u32,
+    test_mod: u128,
     true_target: usize,
     false_target: usize,
-    inspect_count: u32,
+    inspect_count: u128,
 }
 
 fn parse_monkeys(input: &str) -> Vec<RefCell<Monkey>> {
@@ -94,10 +94,10 @@ fn parse_monkeys(input: &str) -> Vec<RefCell<Monkey>> {
             monkeys.push(RefCell::new(Monkey {
                 items: items_line
                     .split(", ")
-                    .map(|x| x.parse::<u32>().unwrap())
+                    .map(|x| x.parse::<u128>().unwrap())
                     .collect(),
                 inspect_op: Op::parse(op_line),
-                test_mod: test_line.parse::<u32>().unwrap(),
+                test_mod: test_line.parse::<u128>().unwrap(),
                 true_target: true_target_line.parse::<usize>().unwrap(),
                 false_target: false_target_line.parse::<usize>().unwrap(),
                 inspect_count: 0,
@@ -108,34 +108,34 @@ fn parse_monkeys(input: &str) -> Vec<RefCell<Monkey>> {
     monkeys
 }
 
-fn take_turn(monkey: &RefCell<Monkey>, monkeys: &Vec<RefCell<Monkey>>) {
-    while monkey.borrow().items.len() > 0 {
+fn take_turn<F: Fn(u128) -> u128>(
+    monkey: &RefCell<Monkey>,
+    monkeys: &[RefCell<Monkey>],
+    post_inspect: F,
+) {
+    while !monkey.borrow().items.is_empty() {
         let mut i = monkey.borrow_mut().items.pop_front().unwrap();
-        i = monkey.borrow().inspect_op.apply(i) / 3;
-        if i % monkey.borrow().test_mod == 0 {
-            monkeys[monkey.borrow().true_target]
-                .borrow_mut()
-                .items
-                .push_back(i);
+        i = monkey.borrow().inspect_op.apply(i);
+        i = post_inspect(i);
+        let target = if i % monkey.borrow().test_mod == 0 {
+            &monkeys[monkey.borrow().true_target]
         } else {
-            monkeys[monkey.borrow().false_target]
-                .borrow_mut()
-                .items
-                .push_back(i);
-        }
+            &monkeys[monkey.borrow().false_target]
+        };
+        target.borrow_mut().items.push_back(i);
         monkey.borrow_mut().inspect_count += 1;
     }
 }
 
-fn do_round(monkeys: &Vec<RefCell<Monkey>>) {
+fn do_round<F: Fn(u128) -> u128>(monkeys: &[RefCell<Monkey>], post_inspect: F) {
     for m in monkeys {
-        take_turn(m, monkeys);
+        take_turn(m, monkeys, &post_inspect);
     }
 }
 
 fn p1(mut monkeys: Vec<RefCell<Monkey>>) {
     for _ in 0..20 {
-        do_round(&monkeys);
+        do_round(&monkeys, |x| x / 3);
     }
 
     monkeys.sort_by(|a, b| b.borrow().inspect_count.cmp(&a.borrow().inspect_count));
@@ -149,6 +149,27 @@ fn p1(mut monkeys: Vec<RefCell<Monkey>>) {
     println!("p1: That's {} monkey business", m0 * m1);
 }
 
+fn p2(mut monkeys: Vec<RefCell<Monkey>>) {
+    let mut d: u128 = 1;
+    for m in &monkeys {
+        d *= m.borrow().test_mod;
+    }
+
+    for _ in 0..10_000 {
+        do_round(&monkeys, |x| x % d);
+    }
+
+    monkeys.sort_by(|a, b| b.borrow().inspect_count.cmp(&a.borrow().inspect_count));
+
+    let m0 = monkeys[0].borrow().inspect_count;
+    let m1 = monkeys[1].borrow().inspect_count;
+    println!(
+        "p2: Two most active monkeys inspected {} and {} times",
+        m0, m1
+    );
+    println!("p2: That's {} monkey business", m0 * m1);
+}
+
 fn main() {
     let mut args = args();
     args.next();
@@ -157,4 +178,5 @@ fn main() {
     let monkeys = parse_monkeys(&input);
 
     p1(monkeys.clone());
+    p2(monkeys);
 }
