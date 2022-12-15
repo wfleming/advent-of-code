db419e425e6c40e2a5809533ed0c993ceb0a4383
Will Fleming <will@flemi.ng>
2022-12-15 14:25:46 -0500

    2022 d15 final
    This was one of those days where actually trying to model the problem
    space as data structures was harmful. Much more efficient to just count
    ranges with math.
    
    This has the slowest runtime this year for me so far by a large margin:
    right around 300ms. This was a challenging one today, so I'm not going
    to spend much more time on optimization.

--- 2022/d15/src/main.rs
+++ 2022/d15/src/main.rs
@@ -1,224 +1,150 @@
-use std::cmp::min;
-use std::collections::BTreeMap;
+use std::cmp::{max, min};
+use std::collections::BTreeSet;
 use std::env::args;
-use std::fmt;
 use std::fs;
-use std::iter;
+use std::ops::RangeInclusive;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Pt {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 impl Pt {
-    pub fn new(x: i32, y: i32) -> Self {
-        Self { x: x, y: y }
+    pub fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
     }
 
-    pub fn dist(&self, other: &Self) -> i32 {
+    pub fn dist(&self, other: &Self) -> i64 {
         (self.x - other.x).abs() + (self.y - other.y).abs()
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq)]
-enum Thing {
-    Sensor(Pt), // the contained pt is the closest beacon, not the pt the sensor itself is at
-    Beacon,
-    NoBeacon,
-    Unknown,
+type Sensors = Vec<(Pt, Pt, i64)>;
+
+fn parse_sensors(input: &str) -> Sensors {
+    let mut sensors = Sensors::new();
+
+    for line in input.lines() {
+        let sx0 = line.find("x=").unwrap() + 2;
+        let sx1 = sx0 + line[sx0..].find(',').unwrap();
+        let sx = line[sx0..sx1].parse::<i64>().unwrap();
+        let sy0 = sx1 + line[sx1..].find("y=").unwrap() + 2;
+        let sy1 = sy0 + line[sy0..].find(':').unwrap();
+        let sy = line[sy0..sy1].parse::<i64>().unwrap();
+
+        let bx0 = sy1 + line[sy1..].find("x=").unwrap() + 2;
+        let bx1 = bx0 + line[bx0..].find(',').unwrap();
+        let bx = line[bx0..bx1].parse::<i64>().unwrap();
+        let by0 = bx1 + line[bx1..].find("y=").unwrap() + 2;
+        let by = line[by0..].parse::<i64>().unwrap();
+
+        let sensor_pt = Pt::new(sx, sy);
+        let beacon_pt = Pt::new(bx, by);
+
+        sensors.push((sensor_pt, beacon_pt, sensor_pt.dist(&beacon_pt)));
+    }
+
+    sensors
 }
 
-#[derive(Clone)]
-struct Cave {
-    data: BTreeMap<Pt, Thing>,
-    sensors: Vec<(Pt, i32)>, // sensors & their range
-    x_min: i32,
-    x_max: i32,
-    y_min: i32,
-    y_max: i32,
+fn blocked_range_in_row(sensor_pt: &Pt, sensor_range: i64, y: i64) -> RangeInclusive<i64> {
+    let y_delta = (sensor_pt.y - y).abs();
+    let x_range = sensor_range - y_delta;
+
+    (sensor_pt.x - x_range)..=(sensor_pt.x + x_range)
 }
 
-impl Cave {
-    pub fn new() -> Cave {
-        Cave {
-            data: BTreeMap::new(),
-            sensors: Vec::new(),
-            x_min: 0,
-            x_max: 0,
-            y_min: 0,
-            y_max: 0,
-        }
-    }
-
-    pub fn parse(input: &str) -> Cave {
-        let mut cave: Cave = Cave::new();
-
-        for line in input.lines() {
-            let sx0 = line.find("x=").unwrap() + 2;
-            let sx1 = sx0 + line[sx0..].find(',').unwrap();
-            let sx = line[sx0..sx1].parse::<i32>().unwrap();
-            let sy0 = sx1 + line[sx1..].find("y=").unwrap() + 2;
-            let sy1 = sy0 + line[sy0..].find(':').unwrap();
-            let sy = line[sy0..sy1].parse::<i32>().unwrap();
-
-            let bx0 = sy1 + line[sy1..].find("x=").unwrap() + 2;
-            let bx1 = bx0 + line[bx0..].find(',').unwrap();
-            let bx = line[bx0..bx1].parse::<i32>().unwrap();
-            let by0 = bx1 + line[bx1..].find("y=").unwrap() + 2;
-            let by = line[by0..].parse::<i32>().unwrap();
-
-            let sensor_pt = Pt::new(sx, sy);
-            let beacon_pt = Pt::new(bx, by);
-            cave.set(&sensor_pt, Thing::Sensor(beacon_pt));
-            cave.set(&beacon_pt, Thing::Beacon);
-            cave.sensors.push((sensor_pt, sensor_pt.dist(&beacon_pt)));
-        }
-
-        cave
-    }
-
-    pub fn get(&self, pt: &Pt) -> &Thing {
-        self.data.get(pt).unwrap_or(&Thing::Unknown)
-    }
-
-    pub fn set(&mut self, pt: &Pt, thing: Thing) {
-        if pt.x < self.x_min {
-            self.x_min = pt.x;
-        }
-        if pt.x > self.x_max {
-            self.x_max = pt.x;
-        }
-        if pt.y < self.y_min {
-            self.y_min = pt.y;
-        }
-        if pt.y > self.y_max {
-            self.y_max = pt.y;
-        }
-
-        self.data.insert(*pt, thing);
-    }
-
-    fn iter(&self) -> CaveIter {
-        CaveIter {
-            cave: self,
-            x: self.x_min,
-            y: self.y_min,
-        }
-    }
-}
-
-impl fmt::Display for Cave {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut s = String::new();
-
-        s.push_str(&format!(
-            "from ({}, {}) to ({}, {})\n",
-            self.x_min, self.y_min, self.x_max, self.y_max
-        ));
-        for (pt, m) in self.iter() {
-            match m {
-                Thing::Sensor(_) => s.push('S'),
-                Thing::Beacon => s.push('B'),
-                Thing::NoBeacon => s.push('x'),
-                Thing::Unknown => s.push('.'),
-            }
-            if pt.x == self.x_max {
-                s.push('\n');
-            }
-        }
-
-        write!(f, "{}", s)
-    }
-}
-
-struct CaveIter<'a> {
-    cave: &'a Cave,
-    x: i32,
-    y: i32,
-}
-
-impl<'a> iter::Iterator for CaveIter<'a> {
-    type Item = (Pt, Thing);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.y > self.cave.y_max {
-            return None;
-        }
-
-        let pt = Pt::new(self.x, self.y);
-        let t = self.cave.get(&pt);
-
-        if self.x == self.cave.x_max {
-            self.x = self.cave.x_min;
-            self.y += 1;
-        } else {
-            self.x += 1;
-        }
-
-        Some((pt, *t))
-    }
-}
-
-fn blocked_pts_in_row(cave: &mut Cave, y_target: i32) -> i32 {
-    let intersecting_sensor_pts: Vec<(Pt, i32)> = cave
-        .sensors
-        .iter()
-        .filter_map(|(pt, range)| {
-            if *range >= (pt.y - y_target).abs() {
-                Some((*pt, *range))
+// we could also merge adjacent ranges (e.g. 2..=4 and 5..=10), but i'm not bothering, it's fine.
+fn merge_ranges(ranges: &mut Vec<RangeInclusive<i64>>) {
+    let mut overlap_found = false;
+    for r1_idx in 0..ranges.len() {
+        let r1 = &ranges[r1_idx];
+        let r2_idx_match = ranges.iter().enumerate().find_map(|(r2_idx, r2)| {
+            if r1_idx != r2_idx
+                && (r1.contains(r2.start())
+                    || r1.contains(r2.end())
+                    || r2.contains(r1.start())
+                    || r2.contains(r1.end()))
+            {
+                Some(r2_idx)
             } else {
                 None
             }
-        })
-        .collect();
+        });
 
-    intersecting_sensor_pts
-        .iter()
-        .fold(0, |acc, (s_pt, range)| {
-            let y_delta = (s_pt.y - y_target).abs();
-            let x_range = range - y_delta;
+        match r2_idx_match {
+            Some(r2_idx) => {
+                let r2 = &ranges[r2_idx];
+                let merged_r = *min(r1.start(), r2.start())..=*max(r1.end(), r2.end());
+                ranges[r1_idx] = merged_r;
+                ranges.remove(r2_idx);
+                overlap_found = true;
+                break;
+            }
+            None => continue,
+        }
+    }
 
-            acc + ((s_pt.x - x_range)..=(s_pt.x + x_range)).fold(0, |row_acc, x| {
-                match cave.get(&Pt::new(x, y_target)) {
-                    Thing::Unknown => {
-                        cave.set(&Pt::new(x, y_target), Thing::NoBeacon);
-                        row_acc + 1
-                    }
-                    _ => row_acc,
-                }
-            })
-        })
+    if overlap_found {
+        merge_ranges(ranges);
+    }
 }
 
-fn p1(cave: &mut Cave, y_target: i32) {
-    let blocked_pts = blocked_pts_in_row(cave, y_target);
+fn blocked_ranges_in_row(sensors: &Sensors, y_target: i64) -> Vec<RangeInclusive<i64>> {
+    let mut ranges: Vec<RangeInclusive<i64>> = Vec::new();
+
+    for (s_pt, _, s_range) in sensors {
+        if s_range >= &(s_pt.y - y_target).abs() {
+            let new_r = blocked_range_in_row(s_pt, *s_range, y_target);
+            ranges.push(new_r.clone());
+            merge_ranges(&mut ranges);
+        }
+    }
+
+    ranges
+}
+
+fn p1(sensors: &Sensors, y_target: i64) {
+    let blocked_ranges = blocked_ranges_in_row(sensors, y_target);
+    let mut blocked_pt_count = blocked_ranges
+        .iter()
+        .fold(0, |accum, r| accum + 1 + (r.end() - r.start()));
+    // p1 only counts squares without something else in them (sensor or beacon), so subtract those
+    let mut discount_pts = BTreeSet::new();
+
+    for (s_pt, b_pt, _) in sensors {
+        if s_pt.y == y_target && blocked_ranges.iter().any(|r| r.contains(&s_pt.x)) {
+            discount_pts.insert(s_pt);
+        } else if b_pt.y == y_target && blocked_ranges.iter().any(|r| r.contains(&b_pt.x)) {
+            discount_pts.insert(b_pt);
+        }
+    }
+    blocked_pt_count -= discount_pts.len() as i64;
     println!(
         "p1: at y == {}, {} positions cannot contain a beacon",
-        y_target, blocked_pts
+        y_target, blocked_pt_count
     );
 }
 
-fn p2(cave: &mut Cave, max_xy: i32) {
-    let x_max = min(cave.x_max, max_xy);
-    let y_max = min(cave.y_max, max_xy);
+fn p2(sensors: &Sensors, max_xy: i64) {
+    for y in 0..=max_xy {
+        let blocked_ranges = blocked_ranges_in_row(sensors, y);
+        let blocked_pt_count = blocked_ranges.iter().fold(0, |accum, r| {
+            let clamped_r_start = max(0, *r.start());
+            let clamped_r_end = min(max_xy, *r.end());
+            accum + 1 + (clamped_r_end - clamped_r_start)
+        });
+        let open_ct = max_xy + 1 - blocked_pt_count;
 
-    for x in 0..=x_max {
-        for y in 0..=y_max {
-            let pt = Pt::new(x, y);
-            match cave.get(&pt) {
-                Thing::Unknown => {
-                    let blocked = cave
-                        .sensors
-                        .iter()
-                        .any(|(s_pt, range)| range >= &s_pt.dist(&pt));
-                    if !blocked {
-                        println!("p2: found unique position at {:?}", &pt);
-                        println!("p2: tuning frequency is {}", (pt.x * 4000000) + pt.y);
-                        return;
-                    }
+        if open_ct == 1 {
+            for x in 0..=max_xy {
+                if !blocked_ranges.iter().any(|r| r.contains(&x)) {
+                    println!("p2: found distress signal at ({},{})", x, y);
+                    let tuning_freq = (x * 4000000) + y;
+                    println!("p2: tuning frequency is {}", tuning_freq);
+                    return;
                 }
-                _ => continue,
             }
         }
     }
@@ -230,15 +156,15 @@ fn main() {
     args.next();
     let input_path = args.next().expect("input filename expected");
     let input = fs::read_to_string(input_path).expect("read the file");
+    let sensors = parse_sensors(&input);
     let p1_y = args
         .next()
         .expect("provide p1 y val")
-        .parse::<i32>()
+        .parse::<i64>()
         .expect("should be a number");
-    let mut cave = Cave::parse(&input);
 
-    p1(&mut cave, p1_y);
+    p1(&sensors, p1_y);
 
     let p2_max = p1_y * 2;
-    p2(&mut cave, p2_max);
+    p2(&sensors, p2_max);
 }
